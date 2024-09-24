5e0bb73a4cd56e16b76b37f466521aeb36c3ae66
IA1phaI <pavel.2014.play@gmail.com>
2024-09-24 19:29:57 +0300

    git autocrlf

---
+++ dot_config/nvim/lua/plugins/neotree.lua
@@ -0,0 +1,27 @@
+return {
+  {
+    "nvim-neo-tree/neo-tree.nvim",
+    branch = "v3.x",
+    dependencies = {
+      "nvim-lua/plenary.nvim",
+      "nvim-tree/nvim-web-devicons", -- not strictly required, but recommended
+      "MunifTanjim/nui.nvim",
+      -- "3rd/image.nvim", -- Optional image support in preview window: See `# Preview Mode` for more information
+    },
+    config = function()
+      vim.fn.sign_define("DiagnosticSignError",
+        { text = " ", texthl = "DiagnosticSignError" })
+      vim.fn.sign_define("DiagnosticSignWarn",
+        { text = " ", texthl = "DiagnosticSignWarn" })
+      vim.fn.sign_define("DiagnosticSignInfo",
+        { text = " ", texthl = "DiagnosticSignInfo" })
+      vim.fn.sign_define("DiagnosticSignHint",
+        { text = "󰌵", texthl = "DiagnosticSignHint" })
+
+      require("neo-tree").setup({})
+
+      vim.keymap.set('n', '<leader>e', ':Neotree toggle focus<CR>')
+      vim.keymap.set('n', '<leader>g', ':Neotree toggle git_status<CR>')
+    end,
+  }
+}
