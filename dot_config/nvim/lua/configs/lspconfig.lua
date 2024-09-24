79ed9caaa1deb8ace0c078143e76952747f1e094
IA1phaI <pavel.2014.play@gmail.com>
2024-09-24 23:55:43 +0300

    nvim lsps

--- dot_config/nvim/lua/configs/lspconfig.lua
+++ dot_config/nvim/lua/configs/lspconfig.lua
@@ -4,7 +4,7 @@ require("nvchad.configs.lspconfig").defaults()
 local lspconfig = require "lspconfig"
 
 -- EXAMPLE
-local servers = { "lua_ls", "html", "cssls", "ts_ls", "java_language_server", "clangd", "pyright", "sqlls", "jsonls", "marksman", "texlab" }
+local servers = { "lua_ls", "html", "cssls", "ts_ls", "java_language_server", "clangd", "pyright", "sqlls", "postgres_lsp", "jsonls", "marksman", "texlab", "bashls" }
 local nvlsp = require "nvchad.configs.lspconfig"
 
 -- lsps with default config
