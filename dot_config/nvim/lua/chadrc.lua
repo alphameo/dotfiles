826127a54de4ce85abca1de4105f7b6581516e9b
IA1phaI <pavel.2014.play@gmail.com>
2024-09-25 00:49:38 +0300

    nvim chadrc

--- dot_config/nvim/lua/chadrc.lua
+++ dot_config/nvim/lua/chadrc.lua
@@ -1,17 +1,37 @@
--- This file needs to have same structure as nvconfig.lua 
+-- This file needs to have same structure as nvconfig.lua
 -- https://github.com/NvChad/ui/blob/v2.5/lua/nvconfig.lua
--- Please read that file to know all available options :( 
+-- Please read that file to know all available options :(
 
 ---@type ChadrcConfig
 local M = {}
 
 M.base46 = {
-	theme = "onedark",
+  theme = "onedark",
+}
 
-	-- hl_override = {
-	-- 	Comment = { italic = true },
-	-- 	["@comment"] = { italic = true },
-	-- },
+M.ui = {
+  cmp = {
+    style = "default", -- default/flat_light/flat_dark/atom/atom_colored
+  },
+
+  telescope = {
+    style = "borderless",  -- borderless / bordered
+  },
+
+  statusline = {
+    theme = "default",                    -- default, vscode, vscode_colored or minimal
+    separator_style = "round",            -- default, round, block or arrow
+    -- default, round, block, and arrow are supported only by the default statusline theme.
+    -- the round and block separators are also supported by the minimal theme.
+  },
+
+  nvdash = {
+    load_on_startup = true,
+  },
+
+  cheatsheet = {
+    theme = "grid", -- simple/grid
+  },
 }
 
 return M
