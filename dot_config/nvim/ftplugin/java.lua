54f546b55e35771d9c0b273c8d67248bbe844de8
IA1phaI <pavel.2014.play@gmail.com>
2024-10-05 10:35:43 +0300

    nvim formatter

--- dot_config/nvim/ftplugin/java.lua
+++ dot_config/nvim/ftplugin/java.lua
@@ -88,10 +88,10 @@ local config = {
 			format = {
 				enabled = false,
 				-- Formatting works by default, but you can refer to a specific file/URL if you choose
-				settings = {
-					url = "https://github.com/google/styleguide/blob/gh-pages/intellij-java-google-style.xml",
-					profile = "GoogleStyle",
-				},
+				-- settings = {
+				-- 	url = "https://github.com/google/styleguide/blob/gh-pages/intellij-java-google-style.xml",
+				-- 	profile = "GoogleStyle",
+				-- },
 			},
 			eclipse = {
 				downloadSources = true,
--- dot_config/nvim/lua/plugins/main/lsp.lua
+++ dot_config/nvim/lua/plugins/main/lsp.lua
@@ -26,7 +26,10 @@ return {
 		"jay-babu/mason-nvim-dap.nvim",
 		config = function()
 			require("mason-nvim-dap").setup({
-				ensure_installed = { "java-debug-adapter", "java-test" },
+				ensure_installed = {
+					"java-debug-adapter",
+					"java-test"
+				},
 			})
 		end,
 	},
