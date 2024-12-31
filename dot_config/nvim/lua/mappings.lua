3fbacf856d8390ddd41d5294f4ea335862ed8e5d
IA1phaI <pavel.2014.play@gmail.com>
2024-12-31 15:32:16 +0300

    nvim: silent mappings

--- dot_config/nvim/lua/mappings.lua
+++ dot_config/nvim/lua/mappings.lua
@@ -19,7 +19,7 @@ map("n", "<leader>q", "<cmd> q! <CR>", { desc = "Force Quit" })
 map("n", "<leader>Q", "<cmd> qall! <CR>", { desc = "Force quit All" })
 
 -- Appearance
-map("n", "<leader>aw", "<cmd>set wrap!<CR>", { desc = "Toggle Line Wrap" })
+map("n", "<leader>aw", "<cmd> set wrap! <CR>", { desc = "Toggle Line Wrap" })
 
 -- Tabs
 -- map('n', '<leader>to', ':tabnew<CR>', { desc = "[T]ab [O]pen" }) -- open new tab
@@ -28,18 +28,18 @@ map("n", "<leader>aw", "<cmd>set wrap!<CR>", { desc = "Toggle Line Wrap" })
 -- map('n', '<leader>tp', ':tabp<CR>', { desc = "[T]ab [P]revious}" }) --  go to previous tab
 
 -- Buffers
-map("n", "<leader>tn", "<cmd> enew <CR>", { desc = "Tab New" }) -- new buffer
-map("n", "<leader>tq", ":Bdelete!<CR>", { desc = "Tab Quit" }) -- close buffer
+map("n", "<leader>tn", "<cmd> enew <CR>", { silent = true, desc = "Tab New" }) -- new buffer
+map("n", "<leader>tq", ":Bdelete!<CR>", { silent = true, desc = "Tab Quit" }) -- close buffer
 
 -- Buffers navigation
-map("n", "<C-Tab>", ":bnext<CR>", { desc = "Next tab (buffer)" })
-map("n", "<C-S-Tab>", ":bprevious<CR>", { desc = "Previous tab (buffer)" })
+map("n", "<C-Tab>", ":bnext<CR>", { silent = true, desc = "Next tab (buffer)" })
+map("n", "<C-S-Tab>", ":bprevious<CR>", { silent = true, desc = "Previous tab (buffer)" })
 
 -- Windows navigation
-map("n", "<A-k>", ":wincmd k<CR>", { desc = "Go to upper window" })
-map("n", "<A-j>", ":wincmd j<CR>", { desc = "Go to lower window" })
-map("n", "<A-h>", ":wincmd h<CR>", { desc = "Go to left window" })
-map("n", "<A-l>", ":wincmd l<CR>", { desc = "Go to right window" })
+map("n", "<A-k>", ":wincmd k<CR>", { silent = true, desc = "Go to upper window" })
+map("n", "<A-j>", ":wincmd j<CR>", { silent = true, desc = "Go to lower window" })
+map("n", "<A-h>", ":wincmd h<CR>", { silent = true, desc = "Go to left window" })
+map("n", "<A-l>", ":wincmd l<CR>", { silent = true, desc = "Go to right window" })
 
 -- Features
 map("v", "<", "<gv", { desc = "Indent left" }) -- stay in visual mode after indent
@@ -59,6 +59,6 @@ map("i", "jk", "<ESC>", { desc = "Exit INSERT MODE" }) -- exit insert mode
 map("i", "kj", "<ESC>", { desc = "Exit INSERT MODE" }) -- exit insert mode
 
 -- Comments
-map("i", "<C-/>", "<ESC>:Commentary<CR>", { desc = "Toggle comment" })
+map("i", "<C-/>", "<ESC>:Commentary<CR>", { silent = true, desc = "Toggle comment" })
 map("n", "<C-/>", "gcc", { remap = true, desc = "Toggle comment" })
 map("v", "<C-/>", "gc", { remap = true, desc = "Toggle comment" })
