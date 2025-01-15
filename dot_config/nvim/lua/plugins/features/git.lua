d49238483dc7a4abccb7592a05bed83df5defaab
alphameo <pavel.2014.play@gmail.com>
2025-01-15 14:40:57 +0300

    feat(nvim): add lazygit plugin

--- dot_config/nvim/lua/plugins/features/git.lua
+++ dot_config/nvim/lua/plugins/features/git.lua
@@ -49,4 +49,24 @@ return {
       vim.keymap.set("n", "<leader>Gp", ":Git push<cr>", { silent = true, desc = "Git Push" })
     end,
   },
+  {
+    "kdheepak/lazygit.nvim",
+    lazy = true,
+    cmd = {
+      "LazyGit",
+      "LazyGitConfig",
+      "LazyGitCurrentFile",
+      "LazyGitFilter",
+      "LazyGitFilterCurrentFile",
+    },
+    -- optional for floating window border decoration
+    dependencies = {
+      "nvim-lua/plenary.nvim",
+    },
+    -- setting the keybinding for LazyGit with 'keys' is recommended in
+    -- order to load the plugin when the command is run for the first time
+    keys = {
+      { "<leader>GG", "<cmd>LazyGit<cr>", desc = "LazyGit" },
+    },
+  },
 }
