35a7dad4a057706a57dac283fea0e5967c2a6db9
alphameo <pavel.2014.play@gmail.com>
2025-01-16 20:30:54 +0300

    refactor(nvim): link markdown keymaps to filetype

--- dot_config/nvim/ftplugin/markdown.lua
+++ dot_config/nvim/ftplugin/markdown.lua
@@ -10,3 +10,6 @@ vim.keymap.set("n", "k", "gk", { silent = true })
 -- Spell check
 vim.opt.spelllang = "en_us"
 vim.opt.spell = true
+
+vim.keymap.set("n", "<leader>lp", ":MarkdownPreviewToggle<CR>", { silent = true, desc = "Markdown Preview" })
+vim.keymap.set("n", "<leader>lr", ":RenderMarkdown toggle<CR>", { silent = true, desc = "Markdown Render" })
--- dot_config/nvim/lua/plugins/lang/markdown.lua
+++ dot_config/nvim/lua/plugins/lang/markdown.lua
@@ -1,13 +1,6 @@
-return {
-  {
-    "neovim/nvim-lspconfig",
-    config = function()
-      require("lspconfig").marksman.setup {
-        capabilities = require("configs.lspconfig").capabilities,
-      }
-    end,
-  },
+require("lspconfig").marksman.setup {}
 
+return {
   {
     "iamcco/markdown-preview.nvim",
     cmd = { "MarkdownPreviewToggle", "MarkdownPreview", "MarkdownPreviewStop" },
@@ -15,22 +8,12 @@ return {
       require("lazy").load { plugins = { "markdown-preview.nvim" } }
       vim.fn["mkdp#util#install"]()
     end,
-    keys = {
-      {
-        "<leader>lp",
-        ft = "markdown",
-        "<cmd>MarkdownPreviewToggle<cr>",
-        desc = "Markdown Preview",
-      },
-    },
     config = function()
       vim.cmd [[do FileType]]
     end,
   },
   {
     "MeanderingProgrammer/render-markdown.nvim",
-    -- dependencies = { "nvim-treesitter/nvim-treesitter", "echasnovski/mini.nvim" }, -- if you use the mini.nvim suite
-    -- dependencies = { 'nvim-treesitter/nvim-treesitter', 'echasnovski/mini.icons' }, -- if you use standalone mini plugins
     dependencies = {
       "nvim-treesitter/nvim-treesitter",
       "nvim-tree/nvim-web-devicons",
@@ -49,7 +32,6 @@ return {
     ft = { "markdown", "norg", "rmd", "org" },
     config = function(_, opts)
       require("render-markdown").setup(opts)
-      vim.keymap.set("n", "<leader>lr", require("render-markdown").toggle, { desc = "Markdown Render" })
     end,
   },
 }
