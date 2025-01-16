bf3533b1430e2fed8dec9642aeaefd6888a18ee4
alphameo <pavel.2014.play@gmail.com>
2025-01-16 19:05:32 +0300

    refactor(nvim): rewrite lsp conf for reverting mason extraction of langs

--- dot_config/nvim/lua/configs/lsp.lua
+++ dot_config/nvim/lua/configs/lsp.lua
@@ -1,38 +1,10 @@
 return {
   {
     "williamboman/mason.nvim",
-    cmd = "Mason",
-    keys = { { "<leader>M", "<cmd>Mason<cr>", desc = "Mason" } },
-    build = ":MasonUpdate",
-    opts_extend = { "ensure_installed" },
-    opts = {
-      ensure_installed = {
-        "stylua",
-        "shfmt",
-      },
-    },
-    ---@param opts MasonSettings | {ensure_installed: string[]}
-    config = function(_, opts)
-      require("mason").setup(opts)
-      local mr = require "mason-registry"
-      mr:on("package:install:success", function()
-        vim.defer_fn(function()
-          -- trigger FileType event to possibly load this newly installed LSP server
-          require("lazy.core.handler.event").trigger {
-            event = "FileType",
-            buf = vim.api.nvim_get_current_buf(),
-          }
-        end, 100)
-      end)
-
-      mr.refresh(function()
-        for _, tool in ipairs(opts.ensure_installed) do
-          local p = mr.get_package(tool)
-          if not p:is_installed() then
-            p:install()
-          end
-        end
-      end)
+    lazy = false,
+    config = function()
+      require("mason").setup()
+      vim.keymap.set({ "n" }, "<leader>M", ":Mason<cr>", { silent = true, desc = "Mason" })
     end,
   },
   {
@@ -41,6 +13,8 @@ return {
       require("mason-lspconfig").setup {
         ensure_installed = {
           "lua_ls",
+          "jdtls",
+          "clangd",
           "kotlin_language_server",
           "html",
           "cssls",
