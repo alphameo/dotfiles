6108f771628c229b6fdb9eb5ddef5c588c4268f4
alphameo <pavel.2014.play@gmail.com>
2025-01-16 21:05:23 +0300

    refactor(nvim): mv cpp lsp setup to file top

--- dot_config/nvim/lua/plugins/lang/ccpp.lua
+++ dot_config/nvim/lua/plugins/lang/ccpp.lua
@@ -1,3 +1,42 @@
+require("lspconfig").clangd.setup {
+  -- on_attach = require("configs.lspconfig").on_attach,
+  -- capabilities = require("configs.lspconfig").capabilities,
+
+  root_dir = function(fname)
+    return require("lspconfig.util").root_pattern(
+      "Makefile",
+      "configure.ac",
+      "configure.in",
+      "config.h.in",
+      "meson.build",
+      "meson_options.txt",
+      "build.ninja"
+    )(fname) or require("lspconfig.util").root_pattern("compile_commands.json", "compile_flags.txt")(fname) or require(
+      "lspconfig.util"
+    ).find_git_ancestor(fname)
+  end,
+
+  capabilities = {
+    offsetEncoding = { "utf-16" },
+  },
+
+  cmd = {
+    "clangd",
+    "--background-index",
+    "--clang-tidy",
+    "--header-insertion=iwyu",
+    "--completion-style=detailed",
+    "--function-arg-placeholders",
+    "--fallback-style=llvm",
+  },
+
+  init_options = {
+    usePlaceholders = true,
+    completeUnimported = true,
+    clangdFileStatus = true,
+  },
+}
+
 return {
   {
     "p00f/clangd_extensions.nvim",
@@ -32,45 +71,6 @@ return {
   {
     "neovim/nvim-lspconfig",
     config = function()
-      require("lspconfig").clangd.setup {
-        -- on_attach = require("configs.lspconfig").on_attach,
-        -- capabilities = require("configs.lspconfig").capabilities,
-
-        root_dir = function(fname)
-          return require("lspconfig.util").root_pattern(
-            "Makefile",
-            "configure.ac",
-            "configure.in",
-            "config.h.in",
-            "meson.build",
-            "meson_options.txt",
-            "build.ninja"
-          )(fname) or require("lspconfig.util").root_pattern("compile_commands.json", "compile_flags.txt")(
-            fname
-          ) or require("lspconfig.util").find_git_ancestor(fname)
-        end,
-
-        capabilities = {
-          offsetEncoding = { "utf-16" },
-        },
-
-        cmd = {
-          "clangd",
-          "--background-index",
-          "--clang-tidy",
-          "--header-insertion=iwyu",
-          "--completion-style=detailed",
-          "--function-arg-placeholders",
-          "--fallback-style=llvm",
-        },
-
-        init_options = {
-          usePlaceholders = true,
-          completeUnimported = true,
-          clangdFileStatus = true,
-        },
-      }
-
       local dap = require "dap"
       if not dap.adapters["codelldb"] then
         require("dap").adapters["codelldb"] = {
