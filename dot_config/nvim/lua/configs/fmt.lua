534287288f09ebc092540fde16009735d71e4e08
alphameo <pavel.2014.play@gmail.com>
2025-01-17 01:12:18 +0300

    feat(nvim): new system of linters and formatters

--- dot_config/nvim/lua/configs/fmt.lua
+++ dot_config/nvim/lua/configs/fmt.lua
@@ -1,75 +1,123 @@
+-- return {
+--   "nvimtools/none-ls.nvim",
+--   dependencies = {
+--     "nvimtools/none-ls-extras.nvim",
+--     "jayp0521/mason-null-ls.nvim", -- ensure dependencies are installed
+--   },
+--   config = function()
+--     local null_ls = require "null-ls"
+--     local formatting = null_ls.builtins.formatting -- to setup formatters
+--     local diagnostics = null_ls.builtins.diagnostics -- to setup linters
+--
+--     -- Formatters & linters for mason to install
+--     require("mason-null-ls").setup {
+--       ensure_installed = {
+--         "prettierd", -- ts/js formatter
+--         "eslint_d", -- ts/js linter
+--         "stylua", -- lua formatter
+--         "shfmt", -- Shell formatter
+--         "checkmake", -- linter for Makefiles
+--         "ktlint",
+--         "cmakelang",
+--         "clang-format",
+--         "cpplint",
+--         "ruff",
+--         "mypy",
+--         "black",
+--         "markdownlint-cli2",
+--       },
+--       automatic_installation = true,
+--     }
+--
+--     local sources = {
+--       formatting.stylua,
+--       formatting.ktlint,
+--       diagnostics.ktlint,
+--       formatting.prettierd.with {
+--         filetypes = {
+--           "css",
+--           "graphql",
+--           "html",
+--           "javascript",
+--           "javascriptreact",
+--           "json",
+--           "less",
+--           "scss",
+--           "typescript",
+--           "typescriptreact",
+--           "yaml",
+--           "markdown",
+--         },
+--       },
+--       require "none-ls.diagnostics.eslint_d",
+--       diagnostics.checkmake,
+--       formatting.shfmt.with { args = { "-i", "4" } },
+--       formatting.clang_format.with {
+--         filetypes = { "c", "cpp" },
+--       },
+--       require "none-ls.diagnostics.cpplint",
+--       diagnostics.mypy,
+--       require("none-ls.formatting.ruff").with { extra_args = { "--extend-select", "I" } },
+--       require "none-ls.formatting.ruff_format",
+--       formatting.black,
+--       diagnostics.markdownlint_cli2,
+--     }
+--
+--     local augroup = vim.api.nvim_create_augroup("LspFormatting", {})
+--     null_ls.setup {
+--       debug = true, -- Enable debug mode. Inspect logs with :NullLsLog.
+--       sources = sources,
+--
+--       vim.keymap.set({ "n", "v", "i" }, "<C-S-i>", vim.lsp.buf.format, { desc = "Code Format" }),
+--       vim.keymap.set({ "n", "v" }, "<leader>cf", vim.lsp.buf.format, { desc = "Code Format" }),
+--     }
+--   end,
+-- }
 return {
-  "nvimtools/none-ls.nvim",
-  dependencies = {
-    "nvimtools/none-ls-extras.nvim",
-    "jayp0521/mason-null-ls.nvim", -- ensure dependencies are installed
-  },
+  "stevearc/conform.nvim",
+  event = { "BufReadPre", "BufNewFile" },
   config = function()
-    local null_ls = require "null-ls"
-    local formatting = null_ls.builtins.formatting -- to setup formatters
-    local diagnostics = null_ls.builtins.diagnostics -- to setup linters
+    local conform = require "conform"
 
-    -- Formatters & linters for mason to install
-    require("mason-null-ls").setup {
-      ensure_installed = {
-        "prettierd", -- ts/js formatter
-        "eslint_d", -- ts/js linter
-        "stylua", -- lua formatter
-        "shfmt", -- Shell formatter
-        "checkmake", -- linter for Makefiles
-        "ktlint",
-        "cmakelang",
-        "clang-format",
-        "cpplint",
-        "ruff",
-        "mypy",
-        "black",
-        "markdownlint-cli2",
+    conform.setup {
+      formatters_by_ft = {
+        javascript = { "prettierd" },
+        typescript = { "prettierd" },
+        javascriptreact = { "prettierd" },
+        typescriptreact = { "prettierd" },
+        graphql = { "prettierd" },
+        css = { "prettierd" },
+        scss = { "prettierd" },
+        html = { "prettierd" },
+        json = { "prettierd" },
+        yaml = { "prettierd" },
+        markdown = { "prettierd" },
+        lua = { "stylua" },
+        python = { "black" },
+        c = { "clang_format" },
+        cpp = { "clang_format" },
+        kotlin = { "ktlint" },
       },
-      automatic_installation = true,
+      --       diagnostics.checkmake,
+      --       formatting.shfmt.with { args = { "-i", "4" } },
+      --       diagnostics.mypy,
+      --       require("none-ls.formatting.ruff").with { extra_args = { "--extend-select", "I" } },
+      --       require "none-ls.formatting.ruff_format",
     }
 
-    local sources = {
-      formatting.stylua,
-      formatting.ktlint,
-      diagnostics.ktlint,
-      formatting.prettierd.with {
-        filetypes = {
-          "css",
-          "graphql",
-          "html",
-          "javascript",
-          "javascriptreact",
-          "json",
-          "less",
-          "scss",
-          "typescript",
-          "typescriptreact",
-          "yaml",
-          "markdown",
-        },
-      },
-      require "none-ls.diagnostics.eslint_d",
-      diagnostics.checkmake,
-      formatting.shfmt.with { args = { "-i", "4" } },
-      formatting.clang_format.with {
-        filetypes = { "c", "cpp" },
-      },
-      require "none-ls.diagnostics.cpplint",
-      diagnostics.mypy,
-      require("none-ls.formatting.ruff").with { extra_args = { "--extend-select", "I" } },
-      require "none-ls.formatting.ruff_format",
-      formatting.black,
-      diagnostics.markdownlint_cli2,
-    }
-
-    local augroup = vim.api.nvim_create_augroup("LspFormatting", {})
-    null_ls.setup {
-      debug = true, -- Enable debug mode. Inspect logs with :NullLsLog.
-      sources = sources,
-
-      vim.keymap.set({ "n", "v", "i" }, "<C-S-i>", vim.lsp.buf.format, { desc = "Code Format" }),
-      vim.keymap.set({ "n", "v" }, "<leader>cf", vim.lsp.buf.format, { desc = "Code Format" }),
-    }
+    vim.keymap.set({ "n", "v", "i" }, "<C-S-i>", function()
+      conform.format {
+        lsp_fallback = true,
+        async = false,
+        timeout_ms = 1000,
+      }
+    end, { desc = "Code Format" })
+    vim.keymap.set({ "n", "v" }, "<leader>cf", function()
+      conform.format {
+        lsp_fallback = true,
+        async = false,
+        timeout_ms = 1000,
+      }
+    end, { desc = "Code Format" })
   end,
 }
