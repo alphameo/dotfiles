234c1bafb30940f63d1a5494bf01d2c550eaa5ac
alphameo <pavel.2014.play@gmail.com>
2025-01-16 21:06:12 +0300

    refactor(nvim): rm comments in ft=markdown

--- dot_config/nvim/ftplugin/markdown.lua
+++ dot_config/nvim/ftplugin/markdown.lua
@@ -1,13 +1,10 @@
--- Markdown specific settings
-vim.opt.wrap = true -- Wrap text
-vim.opt.breakindent = true -- Match indent on line break
-vim.opt.linebreak = true -- Line break on whole words
+vim.opt.wrap = true
+vim.opt.breakindent = true
+vim.opt.linebreak = true
 
--- Allow j/k when navigating wrapped lines
 vim.keymap.set("n", "j", "gj", { silent = true })
 vim.keymap.set("n", "k", "gk", { silent = true })
 
--- Spell check
 vim.opt.spelllang = "en_us"
 vim.opt.spell = true
 
