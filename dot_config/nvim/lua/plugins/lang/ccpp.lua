2b55c71cc9854e10cf6dc7a520dee6e7caa7475e
alphameo <pavel.2014.play@gmail.com>
2025-01-17 13:23:21 +0300

    feat(nvim): add cmake support to cpp

--- dot_config/nvim/lua/plugins/lang/ccpp.lua
+++ dot_config/nvim/lua/plugins/lang/ccpp.lua
@@ -37,6 +37,7 @@ require("lspconfig").clangd.setup {
   },
 }
 
+require("lspconfig").neocmake.setup {}
 return {
   {
     "p00f/clangd_extensions.nvim",
@@ -67,5 +68,28 @@ return {
         },
       },
     },
+  },
+  {
+    "Civitasv/cmake-tools.nvim",
+    lazy = true,
+    init = function()
+      local loaded = false
+      local function check()
+        local cwd = vim.uv.cwd()
+        if vim.fn.filereadable(cwd .. "/CMakeLists.txt") == 1 then
+          require("lazy").load { plugins = { "cmake-tools.nvim" } }
+          loaded = true
+        end
+      end
+      check()
+      vim.api.nvim_create_autocmd("DirChanged", {
+        callback = function()
+          if not loaded then
+            check()
+          end
+        end,
+      })
+    end,
+    opts = {},
   },
 }
