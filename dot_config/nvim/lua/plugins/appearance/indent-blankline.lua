0a80cc714c0e40fe74307ec3125636d462150ca1
alphameo <pavel.2014.play@gmail.com>
2025-01-17 13:25:23 +0300

    feat(nvim): highlight scope

--- dot_config/nvim/lua/plugins/appearance/indent-blankline.lua
+++ dot_config/nvim/lua/plugins/appearance/indent-blankline.lua
@@ -7,7 +7,7 @@ return {
     scope = {
       show_start = false,
       show_end = false,
-      show_exact_scope = false,
+      show_exact_scope = true,
     },
     exclude = {
       filetypes = {
