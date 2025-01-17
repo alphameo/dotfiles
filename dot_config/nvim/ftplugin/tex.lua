f99a42e8d9283a9980d71a47788ba2b7f1fa22b1
alphameo <pavel.2014.play@gmail.com>
2025-01-17 13:20:46 +0300

    refactor(nvim): rename keymap for tex

--- dot_config/nvim/ftplugin/tex.lua
+++ dot_config/nvim/ftplugin/tex.lua
@@ -1,1 +1,1 @@
-vim.keymap.set({ "n" }, "<leader>K", "<plug>(vimtex-doc-package)", { silent = true, desc = "Vimtex Doc Package" })
+vim.keymap.set({ "n" }, "<leader>K", "<plug>(vimtex-doc-package)", { silent = true, desc = "Vimtex Docs" })
