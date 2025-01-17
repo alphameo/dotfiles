904039bf2b82e6bf58c71c3df54e226a1c4584c8
alphameo <pavel.2014.play@gmail.com>
2025-01-17 13:24:14 +0300

    feat(nvim): add json support

---
+++ dot_config/nvim/lua/plugins/lang/json.lua
@@ -0,0 +1,19 @@
+require("lspconfig").jsonls.setup {
+  on_new_config = function(new_config)
+    new_config.settings.json.schemas = new_config.settings.json.schemas or {}
+    vim.list_extend(new_config.settings.json.schemas, require("schemastore").json.schemas())
+  end,
+  settings = {
+    json = {
+      format = {
+        enable = true,
+      },
+      validate = { enable = true },
+    },
+  },
+}
+return {
+  "b0o/SchemaStore.nvim",
+  lazy = true,
+  version = false, -- last release is way too old
+}
