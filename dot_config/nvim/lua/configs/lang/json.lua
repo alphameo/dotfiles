1573c08c5fe4429749c2b078dc28b23ef8b6cab2
alphameo <pavel.2014.play@gmail.com>
2025-01-18 14:30:03 +0300

    refactor(nvim): json conf

--- dot_config/nvim/lua/configs/lang/json.lua
+++ dot_config/nvim/lua/configs/lang/json.lua
@@ -1,14 +1,23 @@
-require("lspconfig").jsonls.setup {
-  on_new_config = function(new_config)
-    new_config.settings.json.schemas = new_config.settings.json.schemas or {}
-    vim.list_extend(new_config.settings.json.schemas, require("schemastore").json.schemas())
-  end,
-  settings = {
-    json = {
-      format = {
-        enable = true,
+local lsp_utils = require "configs.lspconfig"
+
+if lsp_utils.executable "vscode-json-language-server" then
+  require("lspconfig").jsonls.setup {
+    capabilities = lsp_utils.def_cap,
+
+    on_new_config = function(new_config)
+      new_config.settings.json.schemas = new_config.settings.json.schemas or {}
+      vim.list_extend(new_config.settings.json.schemas, require("schemastore").json.schemas())
+    end,
+
+    settings = {
+      json = {
+        format = {
+          enable = true,
+        },
+        validate = { enable = true },
       },
-      validate = { enable = true },
     },
-  },
-}
+  }
+else
+  vim.notify("jsonls not found!", vim.log.levels.WARN, { title = "Nvim-config" })
+end
