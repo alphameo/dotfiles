577a87113057412ddf6c1fbb22fdb033b7903e9e
alphameo <pavel.2014.play@gmail.com>
2025-01-18 14:28:36 +0300

    refactor(nvim): tex conf

--- dot_config/nvim/lua/configs/lang/tex.lua
+++ dot_config/nvim/lua/configs/lang/tex.lua
@@ -1,1 +1,9 @@
-require("lspconfig").texlab.setup {}
+local lsp_utils = require "configs.lspconfig"
+
+if lsp_utils.executable "texlab" then
+  require("lspconfig").texlab.setup {
+    capabilities = lsp_utils.def_cap,
+  }
+else
+  vim.notify("texlab (tex) not found!", vim.log.levels.WARN, { title = "Nvim-config" })
+end
