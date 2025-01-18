bf1d6fac30f37155f293219730393a103d15f710
alphameo <pavel.2014.play@gmail.com>
2025-01-18 14:28:52 +0300

    refactor(nvim): md conf

--- dot_config/nvim/lua/configs/lang/markdown.lua
+++ dot_config/nvim/lua/configs/lang/markdown.lua
@@ -1,1 +1,9 @@
-require("lspconfig").marksman.setup {}
+local lsp_utils = require "configs.lspconfig"
+
+if lsp_utils.executable "marksman" then
+  require("lspconfig").marksman.setup {
+    capabilities = lsp_utils.def_cap,
+  }
+else
+  vim.notify("marksman not found!", vim.log.levels.WARN, { title = "Nvim-config" })
+end
