cf9797e1eee9a21ef5b00875299555f83fbd930b
alphameo <pavel.2014.play@gmail.com>
2025-01-18 23:02:10 +0300

    fix(nvim): ts and js support conf

--- dot_config/nvim/lua/configs/lang/ts-js.lua
+++ dot_config/nvim/lua/configs/lang/ts-js.lua
@@ -1,7 +1,15 @@
 local lsp_utils = require "configs.lspconfig"
 
 if lsp_utils.executable "typescript-language-server" then
-  require("lspconfig").marksman.setup {
+  local function organize_imports()
+    local params = {
+      command = "_typescript.organizeImports",
+      arguments = { vim.api.nvim_buf_get_name(0) },
+    }
+    vim.lsp.buf.execute_command(params)
+  end
+
+  require("lspconfig").ts_ls.setup {
     capabilities = lsp_utils.def_cap,
 
     init_options = {
@@ -12,6 +20,9 @@ if lsp_utils.executable "typescript-language-server" then
           languages = { "javascript", "typescript", "vue" },
         },
       },
+      preferences = {
+        disableSuggestions = true,
+      },
     },
 
     filetypes = {
@@ -19,7 +30,40 @@ if lsp_utils.executable "typescript-language-server" then
       "typescript",
       "vue",
     },
+
+    commands = {
+      OrganizeImports = {
+        organize_imports,
+        description = "Organize Imports",
+      },
+    },
   }
 else
-  vim.notify("ts_ls not found!", vim.log.levels.WARN, { title = "Nvim-config" })
+  vim.notify("ts_ls (TypeScript, JavaScript) not found!", vim.log.levels.WARN, { title = "Nvim-config" })
+end
+
+-- DAP
+
+local dap = require "dap"
+
+dap.adapters["pwa-node"] = {
+  type = "server",
+  host = "127.0.0.1",
+  port = 8123,
+  executable = {
+    command = "js-debug-adapter",
+  },
+}
+
+for _, language in ipairs { "typescript", "javascript", "typescriptreact", "javascriptreact" } do
+  dap.configurations[language] = {
+    {
+      type = "pwa-node",
+      request = "launch",
+      name = "Launch file",
+      program = "${file}",
+      cwd = "${workspaceFolder}",
+      runtimeExecutable = "node",
+    },
+  }
 end
