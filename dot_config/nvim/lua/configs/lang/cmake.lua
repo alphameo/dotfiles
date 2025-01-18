cb014a4d0be08aa2677a1fa1066b127e20614ecb
alphameo <pavel.2014.play@gmail.com>
2025-01-18 14:29:45 +0300

    refactor(nvim): cmake conf

--- dot_config/nvim/lua/configs/lang/cmake.lua
+++ dot_config/nvim/lua/configs/lang/cmake.lua
@@ -1,1 +1,9 @@
-require("lspconfig").neocmake.setup {}
+local lsp_utils = require "configs.lspconfig"
+
+if lsp_utils.executable "neocmakelsp" then
+  require("lspconfig").neocmake.setup {
+    capabilities = lsp_utils.def_cap,
+  }
+else
+  vim.notify("neocmake (cmake) not found!", vim.log.levels.WARN, { title = "Nvim-config" })
+end
