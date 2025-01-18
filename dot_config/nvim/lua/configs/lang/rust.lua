665dda60dc02a0f5646a7e77bb5a250e40364054
alphameo <pavel.2014.play@gmail.com>
2025-01-19 00:54:58 +0300

    refactor(nvim): rm gopher mappings

---
+++ dot_config/nvim/lua/configs/lang/rust.lua
@@ -0,0 +1,13 @@
+local lsp_utils = require "configs.lspconfig"
+
+if not lsp_utils.executable "rust-analyzer" then
+  vim.notify("rust_analyzer (rust) not found!", vim.log.levels.WARN, { title = "Nvim-config" })
+end
+if lsp_utils.executable "bacon-ls" then
+  require("lspconfig").bacon_ls.setup {
+    capabilities = lsp_utils.def_cap,
+    -- enabled = diagnostics == "bacon-ls",
+  }
+else
+  vim.notify("bacon_ls (rust) not found!", vim.log.levels.WARN, { title = "Nvim-config" })
+end
