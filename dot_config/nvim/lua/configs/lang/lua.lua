4205df65957f0d35ac5224d919ab1074f6a252da
alphameo <pavel.2014.play@gmail.com>
2025-01-18 14:28:05 +0300

    refactor(nvim): lua conf

--- dot_config/nvim/lua/configs/lang/lua.lua
+++ dot_config/nvim/lua/configs/lang/lua.lua
@@ -1,24 +1,30 @@
-require("lspconfig").lua_ls.setup {
-  capabilities = require("configs.lspconfig").capabilities,
-  settings = {
-    Lua = {
-      completion = {
-        callSnippet = "Replace",
-      },
-      -- You can toggle below to ignore Lua_LS's noisy `missing-fields` warnings
-      diagnostics = { disable = { "missing-fields" } },
-      runtime = { version = "LuaJIT" },
-      workspace = {
-        checkThirdParty = false,
-        library = {
-          "${3rd}/luv/library",
-          unpack(vim.api.nvim_get_runtime_file("", true)),
+local lsp_utils = require "configs.lspconfig"
+
+if lsp_utils.executable "lua-language-server" then
+  require("lspconfig").lua_ls.setup {
+    capabilities = lsp_utils.def_cap,
+    settings = {
+      Lua = {
+        completion = {
+          callSnippet = "Replace",
         },
+        -- You can toggle below to ignore Lua_LS's noisy `missing-fields` warnings
+        diagnostics = { disable = { "missing-fields" } },
+        runtime = { version = "LuaJIT" },
+        workspace = {
+          checkThirdParty = false,
+          library = {
+            "${3rd}/luv/library",
+            unpack(vim.api.nvim_get_runtime_file("", true)),
+          },
+        },
+        format = {
+          enable = false,
+        },
+        hint = { enable = true },
       },
-      format = {
-        enable = false,
-      },
-      hint = { enable = true },
     },
-  },
-}
+  }
+else
+  vim.notify("lua_ls not found!", vim.log.levels.WARN, { title = "Nvim-config" })
+end
