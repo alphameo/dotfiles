9b50f239e55c1671b7590d4ca6bc077d534b7dff
alphameo <pavel.2014.play@gmail.com>
2025-01-23 00:59:03 +0300

    refactor(nvim): inline imports in cmp

--- dot_config/nvim/lua/plugins/autopairs.lua
+++ dot_config/nvim/lua/plugins/autopairs.lua
@@ -5,10 +5,7 @@ return {
     "hrsh7th/nvim-cmp",
   },
   config = function()
-    local autopairs = require "nvim-autopairs"
-    autopairs.setup {}
-    local cmp_autopairs = require "nvim-autopairs.completion.cmp"
-    local cmp = require "cmp"
-    cmp.event:on("confirm_done", cmp_autopairs.on_confirm_done())
+    require("nvim-autopairs").setup {}
+    require("cmp").event:on("confirm_done", require("nvim-autopairs.completion.cmp").on_confirm_done())
   end,
 }
