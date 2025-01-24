dafc0ca685d1cd6f1cea3130c31846d02faf9057
alphameo <pavel.2014.play@gmail.com>
2025-01-24 16:09:32 +0300

    refactor(nvim): oprim via local vars + change accessors in options

--- dot_config/nvim/lua/options.lua
+++ dot_config/nvim/lua/options.lua
@@ -1,43 +1,58 @@
-vim.wo.number = true -- Make line numbers default (default: false)
-vim.opt.nu = true
-vim.opt.relativenumber = true -- Set relative numbered lines (default: false)
+local opt = vim.opt
+local o = vim.o
+local g = vim.g
 
-vim.opt.tabstop = 2 -- Insert n spaces for a tab (default: 8)
-vim.opt.softtabstop = 2 -- Number of spaces that a tab counts for while performing editing operations (default: 0)
-vim.opt.shiftwidth = 2 -- The number of spaces inserted for each indentation (default: 8)
-vim.opt.expandtab = true -- Convert tabs to spaces (default: false)
+o.number = true -- Make line numbers default (default: false)
+opt.nu = true
+opt.relativenumber = true -- Set relative numbered lines (default: false)
 
-vim.opt.smartindent = true
-vim.opt.autoindent = true -- Copy indent from current line when starting new one (default: true)
-vim.opt.breakindent = true -- Enable break indent (default: false)
+opt.tabstop = 4 -- Insert n spaces for a tab (default: 8)
+opt.softtabstop = 4 -- Number of spaces that a tab counts for while performing editing operations (default: 0)
+opt.shiftwidth = 4 -- The number of spaces inserted for each indentation (default: 8)
+opt.expandtab = true -- Convert tabs to spaces (default: false)
+opt.smartindent = true
+opt.autoindent = true -- Copy indent from current line when starting new one (default: true)
+opt.breakindent = true -- Enable break indent (default: false)
 
-vim.opt.wrap = false -- Display lines as one long line (default: true)
-vim.opt.linebreak = true -- Companion to wrap, don't split words (default: false)
+opt.wrap = false -- Display lines as one long line (default: true)
+opt.linebreak = true -- Companion to wrap, don't split words (default: false)
 
-vim.opt.swapfile = false -- Creates a swapfile (default: true)
-vim.opt.backup = false -- Creates a backup file (default: false)
-vim.opt.undofile = true -- Save undo history (default: false)
-vim.opt.undodir = os.getenv "HOME" .. "/.vim/undodir"
+opt.swapfile = false -- Creates a swapfile (default: true)
+opt.backup = false -- Creates a backup file (default: false)
+opt.undofile = true -- Save undo history (default: false)
+opt.undodir = os.getenv "HOME" .. "/.vim/undodir"
 
-vim.opt.hlsearch = false -- Set highlight on search (default: true)
-vim.opt.incsearch = true
-vim.opt.ignorecase = true -- Case-insensitive searching UNLESS \C or capital in search (default: false)
-vim.opt.smartcase = true -- Smart case (default: false)
+opt.hlsearch = false -- Set highlight on search (default: true)
+opt.incsearch = true
+opt.ignorecase = true -- Case-insensitive searching UNLESS \C or capital in search (default: false)
+opt.smartcase = true -- Smart case (default: false)
 
-vim.opt.termguicolors = true -- Set termguicolors to enable highlight groups (default: false)
+opt.termguicolors = true -- Set termguicolors to enable highlight groups (default: false)
 
-vim.opt.scrolloff = 8 -- Minimal number of screen lines to keep above and below the cursor (default: 0)
-vim.opt.sidescrolloff = 8 -- Minimal number of screen columns either sideof cursor if wrap is `false` (default: 0)
-vim.opt.signcolumn = "yes" -- Keep signcolumn on by default (default: 'auto')
-vim.opt.isfname:append "@-@"
+opt.scrolloff = 8 -- Minimal number of screen lines to keep above and below the cursor (default: 0)
+opt.sidescrolloff = 8 -- Minimal number of screen columns either sideof cursor if wrap is `false` (default: 0)
+opt.signcolumn = "yes" -- Keep signcolumn on by default (default: 'auto')
+opt.isfname:append "@-@"
 
-vim.opt.updatetime = 50 -- Decrease update time (default: 4000)
+opt.updatetime = 50 -- Decrease update time (default: 4000)
 
-vim.opt.clipboard = "unnamedplus" -- Sync clipboard between OS and Neovim. (default: '')
-vim.opt.mouse = "a" -- Enable mouse mode (default: '')
+opt.clipboard = "unnamedplus" -- Sync clipboard between OS and Neovim. (default: '')
+opt.mouse = "a" -- Enable mouse mode (default: '')
 
-vim.opt.showmode = false -- We don't need to see things like -- INSERT -- anymore. We have in lualine plugin (default: true)
-vim.opt.showtabline = 2 -- Always show tabs (default: 1)
-vim.opt.conceallevel = 0 -- So that `` is visible in markdown files (default: 1)
+o.showmode = false -- We don't need to see things like -- INSERT -- anymore. We have in lualine plugin (default: true)
+opt.showtabline = 2 -- Always show tabs (default: 1)
+opt.conceallevel = 0 -- So that `` is visible in markdown files (default: 1)
 
-vim.opt.pumheight = 10 -- Pop up menu height (default: 0)
+opt.pumheight = 10 -- Pop up menu height (default: 0)
+
+-- disable some default providers
+g.loaded_node_provider = 0
+g.loaded_python3_provider = 0
+g.loaded_perl_provider = 0
+g.loaded_ruby_provider = 0
+
+-- add binaries installed by mason.nvim to path
+local is_windows = vim.fn.has "win32" ~= 0
+local sep = is_windows and "\\" or "/"
+local delim = is_windows and ";" or ":"
+vim.env.PATH = table.concat({ vim.fn.stdpath "data", "mason", "bin" }, sep) .. delim .. vim.env.PATH
