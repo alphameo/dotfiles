ccd7cd4973d2c581a9c039bbc6c71eb0b3c0c484
alphameo <pavel.2014.play@gmail.com>
2025-01-30 11:59:27 +0300

    refactor(nvim): cmake, c and cpp mappings

--- dot_config/nvim/ftplugin/cmake.lua
+++ dot_config/nvim/ftplugin/cmake.lua
@@ -1,5 +1,4 @@
 local map = vim.keymap.set
 
 map({ "n" }, "<leader>lG", ":CMakeGenerate<CR>", { silent = true, desc = "CMake Generate Project" })
-map({ "n" }, "<leader>lS", ":CMakeSettings<CR>", { silent = true, desc = "CMake Settings Project" })
-map({ "n" }, "<leader>lR", ":CMakeSelectCwd<CR>", { silent = true, desc = "CMake Select Root" })
+map({ "n" }, "<leader>l.", ":CMakeSelectCwd<CR>", { silent = true, desc = "CMake Select Root" })
