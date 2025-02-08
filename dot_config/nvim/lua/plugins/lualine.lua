1c76e4c087472e807480d57d0b3f269fae0ebbbf
alphameo <pavel.2014.play@gmail.com>
2025-02-08 18:07:58 +0300

    refactor(nvim): add keystroke display to lualine

--- dot_config/nvim/lua/plugins/lualine.lua
+++ dot_config/nvim/lua/plugins/lualine.lua
@@ -52,6 +52,16 @@ return {
         lualine_b = { "branch" },
         lualine_c = { filename },
         lualine_x = {
+          {
+            require("noice").api.status.command.get,
+            cond = require("noice").api.status.command.has,
+            color = { fg = "#ff9e64" },
+          },
+          {
+            require("noice").api.status.mode.get,
+            cond = require("noice").api.status.mode.has,
+            color = { fg = "#ff9e64" },
+          },
           diagnostics,
           diff,
           { "encoding", cond = hide_in_width },
