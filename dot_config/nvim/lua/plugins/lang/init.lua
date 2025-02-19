f9c639812d79391730e381f2cbb8c69c6f064d00
alphameo <pavel.2014.play@gmail.com>
2025-02-20 00:23:26 +0300

    refactor(nvim): rm dep for deleted json/yaml plugin

--- dot_config/nvim/lua/plugins/lang/init.lua
+++ dot_config/nvim/lua/plugins/lang/init.lua
@@ -3,7 +3,6 @@ return {
   require "plugins.lang.cmake",
   require "plugins.lang.go",
   require "plugins.lang.java",
-  require "plugins.lang.json-yaml",
   require "plugins.lang.markdown",
   require "plugins.lang.python",
   require "plugins.lang.rust",
