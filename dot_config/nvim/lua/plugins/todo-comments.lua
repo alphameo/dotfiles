49200a67535fb7e79aae95c2419625f7ba924afa
alphameo <pavel.2014.play@gmail.com>
2025-02-20 00:24:48 +0300

    refactor(nvim): add TODO side signs and mappings

--- dot_config/nvim/lua/plugins/todo-comments.lua
+++ dot_config/nvim/lua/plugins/todo-comments.lua
@@ -2,5 +2,11 @@ return {
   "folke/todo-comments.nvim",
   event = "VimEnter",
   dependencies = { "nvim-lua/plenary.nvim" },
-  opts = { signs = false },
+  opts = { signs = true },
+  -- stylua: ignore
+  config = function()
+    require("todo-comments").setup()
+    vim.keymap.set("n", "<leader>ft", ":TodoTelescope<cr>", { silent = true, desc = "Find TODOs" })
+    vim.keymap.set("n", "<leader>fT", ":TodoTelescope keywords=TODO,FIX,FIXME<cr>", { silent = true, desc = "Find TODO/FIX/..." })
+  end,
 }
