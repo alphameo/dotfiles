1aa66ea902694d839653b5699b19f01983210421
alphameo <pavel.2014.play@gmail.com>
2025-02-20 00:25:38 +0300

    refactor(nvim): upd langmapper cfg

--- dot_config/nvim/lua/plugins/langmapper.lua
+++ dot_config/nvim/lua/plugins/langmapper.lua
@@ -1,11 +1,29 @@
 return {
   "Wansmer/langmapper.nvim",
+  cond = true,
+  name = "langmapper",
+  main = "langmapper",
   lazy = false,
-  priority = 1, -- High priority is needed if you will use `autoremap()`
+  priority = 1,
   config = function()
     local langmapper = require "langmapper"
 
     langmapper.setup {}
     langmapper.automapping { global = true, buffer = true }
+
+    local function escape(str)
+      local escape_chars = [[;,."|\]]
+      return vim.fn.escape(str, escape_chars)
+    end
+
+    local en = [[`qwertyuiop[]asdfghjkl;'zxcvbnm]]
+    local ru = [[ёйцукенгшщзхъфывапролджэячсмить]]
+    local en_shift = [[~QWERTYUIOP{}ASDFGHJKL:"ZXCVBNM<>]]
+    local ru_shift = [[ËЙЦУКЕНГШЩЗХЪФЫВАПРОЛДЖЭЯЧСМИТЬБЮ]]
+
+    vim.opt.langmap = vim.fn.join({
+      escape(ru_shift) .. ";" .. escape(en_shift),
+      escape(ru) .. ";" .. escape(en),
+    }, ",")
   end,
 }
