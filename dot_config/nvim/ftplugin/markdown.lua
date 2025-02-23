892664c2943d7f5ede574895d9ca29dd27299298
alphameo <pavel.2014.play@gmail.com>
2025-02-24 00:29:17 +0300

    refactor(nvim): spelling

--- dot_config/nvim/ftplugin/markdown.lua
+++ dot_config/nvim/ftplugin/markdown.lua
@@ -4,7 +4,10 @@ local opt = vim.opt
 opt.wrap = true
 opt.breakindent = true
 opt.linebreak = true
-opt.spelllang = "en_us"
+opt.spelllang = {
+  "en_US",
+  "ru_RU",
+}
 opt.spell = true
 
 map("n", "j", "gj", { silent = true })
@@ -16,11 +19,11 @@ map("n", "<leader>lr", ":RenderMarkdown toggle<CR>", { silent = true, desc = "Ma
 map("n", "<leader>li", ":MoltenInit<CR>", { silent = true, desc = "Molten Initialize" })
 map("n", "<leader>le", ":MoltenEvaluateOperator<CR>", { silent = true, desc = "Molten Operator Selection" })
 map("n", "<leader>ll", ":MoltenEvaluateLine<CR>", { silent = true, desc = "Molten Evaluate Line" })
-map("n", "<leader>lr", ":MoltenReevaluateCell<CR>", { silent = true, desc = "Molten Re-evaluate Cell" })
+map("n", "<leader>lE", ":MoltenReevaluateCell<CR>", { silent = true, desc = "Molten Re-evaluate Cell" })
 map("n", "<leader>ld", ":MoltenDelete<CR>", { silent = true, desc = "Molten Delete Cell" })
 map("n", "<leader>lh", ":MoltenHideOutput<CR>", { silent = true, desc = "Molten Hide Output" })
 map("n", "<leader>ls", ":noautocmd MoltenEnterOutput<CR>", { silent = true, desc = "Molten Show/Enter Output" })
-map("v", "<leader>lr", ":<C-u>MoltenEvaluateVisual<CR>gv", { silent = true, desc = "Evaluate visual selection" })
+map("v", "<leader>lE", ":<C-u>MoltenEvaluateVisual<CR>gv", { silent = true, desc = "Evaluate visual selection" })
 
 map("n", "<leader>lI", function()
   local venv = os.getenv "VIRTUAL_ENV" or os.getenv "CONDA_PREFIX"
@@ -34,14 +37,14 @@ map("n", "<leader>lI", function()
 end, { desc = "Molten Initialize for python3", silent = true })
 
 local quarto = require "quarto"
-map("n", "<leader>lI", ":QuartoActivate<CR>", { silent = true, desc = "Quatro Initialize" })
-map("n", "<leader>lP", quarto.quartoPreview, { silent = true, desc = "Quatro Preview" })
+map("n", "<leader>lI", ":QuartoActivate<CR>", { silent = true, desc = "Quarto Initialize" })
+map("n", "<leader>lP", quarto.quartoPreview, { silent = true, desc = "Quarto Preview" })
 local runner = require "quarto.runner"
-map("n", "<leader>lc", runner.run_cell, { silent = true, desc = "Quatro Run Cell" })
-map("n", "<leader>lu", runner.run_above, { silent = true, desc = "Quatro Run Cell And Upper" })
-map("n", "<leader>la", runner.run_all, { silent = true, desc = "Quatro Run All Cells" })
-map("n", "<leader>lL", runner.run_line, { silent = true, desc = "Quatro Run Line" })
+map("n", "<leader>lc", runner.run_cell, { silent = true, desc = "Quarto Run Cell" })
+map("n", "<leader>lu", runner.run_above, { silent = true, desc = "Quarto Run Cell And Upper" })
+map("n", "<leader>la", runner.run_all, { silent = true, desc = "Quarto Run All Cells" })
+map("n", "<leader>lL", runner.run_line, { silent = true, desc = "Quarto Run Line" })
 map("n", "<leader>lA", function()
   runner.run_all(true)
-end, { desc = "Quatro Run All Cells of All Languages", silent = true })
-map("v", "<leader>lr", runner.run_range, { silent = true, desc = "Quatro Run Visual Range" })
+end, { desc = "Quarto Run All Cells of All Languages", silent = true })
+map("v", "<leader>lr", runner.run_range, { silent = true, desc = "Quarto Run Visual Range" })
