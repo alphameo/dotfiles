926a6b59a64dd7122cebeb59ebac7a9a79c88746
alphameo <pavel.2014.play@gmail.com>
2025-02-28 19:11:42 +0300

    feat(nvim): add tex filetype settings

--- dot_config/nvim/ftplugin/tex.lua
+++ dot_config/nvim/ftplugin/tex.lua
@@ -1,1 +1,15 @@
-vim.keymap.set({ "n" }, "<leader>K", "<plug>(vimtex-doc-package)", { silent = true, desc = "Vimtex Docs" })
+local map = vim.keymap.set
+local opt = vim.opt
+
+opt.wrap = true
+opt.breakindent = true
+opt.linebreak = true
+opt.spelllang = {
+  "en_US",
+  "ru_RU",
+}
+
+opt.spell = true
+map({ "n" }, "<leader>K", "<plug>(vimtex-doc-package)", { silent = true, desc = "Vimtex Docs" })
+map("n", "j", "gj", { silent = true })
+map("n", "k", "gk", { silent = true })
