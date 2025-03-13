a151ccd8e2310f950c3cfe0e911d6cdbb403dccb
alphameo <pavel.2014.play@gmail.com>
2025-03-13 22:57:26 +0300

    fix(nvim): clang format flags

--- dot_config/nvim/lua/configs/lang/ccpp.lua
+++ dot_config/nvim/lua/configs/lang/ccpp.lua
@@ -60,18 +60,19 @@ local conform = require "conform"
 conform.formatters_by_ft.c = { "clang_format" }
 conform.formatters_by_ft.cpp = { "clang_format" }
 conform.formatters = {
-  ["clang-format"] = {
-    prepend_args = {
-      "-style={ \
-                BasedOnStyle: Google, \
-                UseTab: Never, \
-                TabWidth: 4, \
-                indentWidth: 4, \
-                InsertNewlineAtEOF: true, \
-                IndentCaseLabels: true, \
-                BreakBeforeBraces: Attach, \
-                AllowShortIfStatementsOnASingleLine: false, \
-                AllowShortBlocksOnASingleLine: Empty}",
+  clang_format = {
+    args = {
+      "--style={BasedOnStyle: Google,"
+        .. "UseTab: Never,"
+        .. "TabWidth: 4,"
+        .. "IndentWidth: 4,"
+        .. "InsertNewlineAtEOF: true,"
+        .. "IndentCaseLabels: true,"
+        .. "LineEnding: LF,"
+        .. "BreakBeforeBraces: Attach,"
+        .. "AllowShortIfStatementsOnASingleLine: false,"
+        .. "AllowShortBlocksOnASingleLine: Empty"
+        .. "}",
     },
   },
 }
