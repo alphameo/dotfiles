e8e0ce52fddfdf07b691b165a3b640a6ad8fdfd0
alphameo <pavel.2014.play@gmail.com>
2025-03-30 21:56:56 +0300

    feat(nvim, zsh): default configs for eslint_d and prettierd

---
+++ dot_config/nvim/utils/eslintrc.js
@@ -0,0 +1,19 @@
+module.exports = {
+  rules: {
+    "no-console": "warn",
+    indent: ["error", 2],
+    quotes: ["error", "single"],
+    "no-unused-vars": "warn",
+    "no-undef": "error",
+    "no-trailing-spaces": "error",
+    semi: ["error", "always"],
+    curly: "error",
+    eqeqeq: "error",
+    "no-alert": "warn",
+    "no-debugger": "error",
+  },
+  // env: {
+  //   node: true,
+  //   browser: true,
+  // },
+};
