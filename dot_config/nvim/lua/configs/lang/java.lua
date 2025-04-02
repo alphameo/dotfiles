6a10ba5b2dee3eaf10ed47615a6ed20b4c5f248d
alphameo <pavel.2014.play@gmail.com>
2025-04-02 15:47:22 +0300

    fix(nvim): fmt of cfg java

--- dot_config/nvim/lua/configs/lang/java.lua
+++ dot_config/nvim/lua/configs/lang/java.lua
@@ -1,21 +1,21 @@
 -- INFO: LSP
-local lsp_utils = require "configs.lspconfig"
+local lsp_utils = require("configs.lspconfig")
 
-if not lsp_utils.executable "jdtls" then
-  vim.notify("jdtls (Java) not found!", vim.log.levels.WARN, { title = "Nvim-config" })
+if not lsp_utils.executable("jdtls") then
+	vim.notify("jdtls (Java) not found!", vim.log.levels.WARN, { title = "Nvim-config" })
 end
 
 -- INFO: DAP
-local dap = require "dap"
+local dap = require("dap")
 
 dap.configurations.java = {
-  {
-    type = "java",
-    request = "attach",
-    name = "Debug (Attach) - Remote",
-    hostName = "127.0.0.1",
-    port = 5005,
-  },
+	{
+		type = "java",
+		request = "attach",
+		name = "Debug (Attach) - Remote",
+		hostName = "127.0.0.1",
+		port = 5005,
+	},
 }
 
 -- INFO: FORMATTING
