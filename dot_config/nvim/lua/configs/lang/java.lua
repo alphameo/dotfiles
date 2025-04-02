f9aa9809b6455ae5b6a5c21b4a812797d28295e7
alphameo <pavel.2014.play@gmail.com>
2025-04-02 16:37:45 +0300

    refactor(nvim): mv jdtls config from ftplugin to configs.lang.java

--- dot_config/nvim/lua/configs/lang/java.lua
+++ dot_config/nvim/lua/configs/lang/java.lua
@@ -1,21 +1,167 @@
 -- INFO: LSP
-local lsp_utils = require("configs.lspconfig")
+local lsp_utils = require "configs.lspconfig"
 
-if not lsp_utils.executable("jdtls") then
-	vim.notify("jdtls (Java) not found!", vim.log.levels.WARN, { title = "Nvim-config" })
-end
+local jdtls = require "jdtls"
+local project_name = vim.fn.fnamemodify(vim.fn.getcwd(), ":p:h:t")
+local workspace_dir = vim.env.HOME .. "/jdtls-workspace/" .. project_name
+
+local bundles = {
+  vim.fn.glob(vim.env.HOME .. "/.local/share/nvim/mason/share/java-debug-adapter/com.microsoft.java.debug.plugin.jar"),
+}
+
+vim.list_extend(
+  bundles,
+  vim.split(vim.fn.glob(vim.env.HOME .. "/.local/share/nvim/mason/share/java-test/*.jar", 1), "\n")
+)
+
+local config = {
+  cmd = {
+    "java",
+    "-Declipse.application=org.eclipse.jdt.ls.core.id1",
+    "-Dosgi.bundles.defaultStartLevel=4",
+    "-Declipse.product=org.eclipse.jdt.ls.core.product",
+    "-Dlog.protocol=true",
+    "-Dlog.level=ALL",
+    "-Xmx1g",
+    -- "-Xmx4g",
+    "--add-modules=ALL-SYSTEM",
+    "--add-opens",
+    "java.base/java.util=ALL-UNNAMED",
+    "--add-opens",
+    "java.base/java.lang=ALL-UNNAMED",
+    "-javaagent:" .. vim.env.HOME .. "/.local/share/nvim/mason/share/jdtls/lombok.jar",
+
+    "-jar",
+    vim.env.HOME .. "/.local/share/nvim/mason/share/jdtls/plugins/org.eclipse.equinox.launcher.jar",
+    -- TODO: jdtls subdirectory for the OS (config_linux, config_mac, config_win, etc)
+    "-configuration",
+    vim.env.HOME .. "/.local/share/nvim/mason/packages/jdtls/config_linux",
+    "-data",
+    workspace_dir,
+  },
+
+  root_dir = require("jdtls.setup").find_root { ".git", "mvnw", "gradlew", "pom.xml", "build.gradle" },
+
+  settings = {
+    java = {
+      -- TODO: path to the main java version (JDK 17 or higher)
+      home = "/usr/lib/jvm/java-23-openjdk",
+
+      configuration = {
+        updateBuildConfiguration = "interactive",
+        runtimes = {
+          {
+            name = "JavaSE-23",
+            path = "/usr/lib/jvm/java-23-openjdk",
+          },
+        },
+      },
+      format = {
+        enabled = true,
+        settings = {
+          -- url = "https://github.com/google/styleguide/blob/gh-pages/intellij-java-google-style.xml",
+          -- path = "$HOME/.config/nvim//utility/styles/intellij-java-google-style.xml",
+          -- url = "https://github.com/google/styleguide/blob/gh-pages/eclipse-java-google-style.xml",
+          path = "$HOME/.config/nvim/utility/styles/eclipse-java-google-style.xml",
+          profile = "GoogleStyle",
+        },
+      },
+      eclipse = {
+        downloadSources = true,
+      },
+      maven = {
+        downloadSources = true,
+      },
+      signatureHelp = {
+        enabled = true,
+      },
+      contentProvider = {
+        preferred = "fernflower",
+      },
+      saveActions = {
+        organizeImports = true,
+      },
+      completion = {
+        favoriteStaticMembers = {
+          "org.hamcrest.MatcherAssert.assertThat",
+          "org.hamcrest.Matchers.*",
+          "org.hamcrest.CoreMatchers.*",
+          "org.junit.jupiter.api.Assertions.*",
+          "java.util.Objects.requireNonNull",
+          "java.util.Objects.requireNonNullElse",
+          "org.mockito.Mockito.*",
+        },
+        filteredTypes = {
+          "com.sun.*",
+          "io.micrometer.shaded.*",
+          "java.awt.*",
+          "jdk.*",
+          "sun.*",
+        },
+        importOrder = {
+          "java",
+          "javax",
+          "com",
+          "org",
+        },
+      },
+      extendedClientCapabilities = jdtls.extendedClientCapabilities,
+      sources = {
+        organizeImports = {
+          starThreshold = 9999,
+          staticStarThreshold = 9999,
+        },
+      },
+      codeGeneration = {
+        toString = {
+          template = "${object.className}{${member.name()}=${member.value}, ${otherMembers}}",
+        },
+        hashCodeEquals = {
+          useJava7Objects = true,
+        },
+        useBlocks = true,
+      },
+      implementationsCodeLens = {
+        enabled = true,
+      },
+      referencesCodeLens = {
+        enabled = true,
+      },
+      inlayHints = {
+        parameterNames = {
+          enabled = "all",
+        },
+      },
+      references = {
+        includeDecompiledSources = true,
+      },
+    },
+  },
+  capabilities = lsp_utils.capabilities,
+  flags = {
+    allow_incremental_sync = true,
+  },
+  init_options = {
+    bundles = bundles,
+  },
+
+  on_attach = function(client, bufnr)
+    jdtls.setup_dap { hotcodereplace = "auto" }
+    require("jdtls.dap").setup_dap_main_class_configs()
+  end,
+}
 
 -- INFO: DAP
-local dap = require("dap")
+local dap = require "dap"
 
 dap.configurations.java = {
-	{
-		type = "java",
-		request = "attach",
-		name = "Debug (Attach) - Remote",
-		hostName = "127.0.0.1",
-		port = 5005,
-	},
+  {
+    type = "java",
+    request = "attach",
+    name = "Debug (Attach) - Remote",
+    hostName = "127.0.0.1",
+    port = 5005,
+  },
 }
 
 -- INFO: FORMATTING
@@ -23,3 +169,10 @@ dap.configurations.java = {
 
 -- INFO: LINTING
 -- check jdtls
+
+-- INFO: start function for ftplugin
+local M = {}
+M.start = function()
+  jdtls.start_or_attach(config)
+end
+return M
