9a94d99898a8661073449d74435cc9fe20f39795
alphameo <pavel.2014.play@gmail.com>
2025-04-02 21:25:50 +0300

    refactor(nvim): organize todo-comments keymaps

--- dot_config/nvim/lua/plugins/todo-comments.lua
+++ dot_config/nvim/lua/plugins/todo-comments.lua
@@ -3,10 +3,11 @@ return {
   event = "VimEnter",
   dependencies = { "nvim-lua/plenary.nvim" },
   opts = { signs = true },
-  -- stylua: ignore
   config = function()
     require("todo-comments").setup()
-    vim.keymap.set("n", "<leader>ft", ":TodoTelescope<cr>", { silent = true, desc = "Find TODOs" })
-    vim.keymap.set("n", "<leader>fT", ":TodoTelescope keywords=TODO,FIX,FIXME<cr>", { silent = true, desc = "Find TODO/FIX/..." })
+
+    local map = vim.keymap.set
+    map("n", "<leader>ft", ":TodoTelescope<cr>", { silent = true, desc = "Find TODOs" })
+    map("n", "<leader>fT", ":TodoTelescope keywords=TODO,FIX,FIXME<cr>", { silent = true, desc = "Find TODO/FIX/..." })
   end,
 }
