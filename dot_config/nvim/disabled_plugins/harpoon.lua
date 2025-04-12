8ced1cd0db7c26575c668b713f2f418efbe6f635
alphameo <pavel.2014.play@gmail.com>
2025-04-13 01:32:11 +0300

    feat(nvim): add old plugins specs

---
+++ dot_config/nvim/disabled_plugins/harpoon.lua
@@ -0,0 +1,37 @@
+return {
+  "ThePrimeagen/harpoon",
+  event = "VeryLazy",
+  branch = "harpoon2",
+  opts = {
+    menu = {
+      width = vim.api.nvim_win_get_width(0) - 4,
+    },
+    settings = {
+      save_on_toggle = true,
+    },
+  },
+
+  keys = function()
+    local harpoon = require "harpoon"
+    local keys = {
+      {
+        "<leader>bf",
+        function()
+          harpoon:list():add()
+        end,
+        silent = true,
+        desc = "Harpoon File",
+      },
+      {
+        "<leader>bb",
+        function()
+          harpoon.ui:toggle_quick_menu(harpoon:list())
+        end,
+        silent = true,
+        desc = "Harpoon Menu",
+      },
+    }
+
+    return keys
+  end,
+}
