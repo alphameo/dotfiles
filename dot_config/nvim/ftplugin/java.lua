61d3c861b3fba897ddc8c824e4bd15babe044e5b
alphameo <pavel.2014.play@gmail.com>
2025-04-21 23:28:12 +0300

    chore(nvim): jdtls mappings to map alias func

--- dot_config/nvim/ftplugin/java.lua
+++ dot_config/nvim/ftplugin/java.lua
@@ -5,12 +5,13 @@ vim.cmd "command! -buffer JdtUpdateConfig lua require('jdtls').update_project_co
 vim.cmd "command! -buffer JdtBytecode lua require('jdtls').javap()"
 vim.cmd "command! -buffer JdtJshell lua require('jdtls').jshell()"
 
-vim.keymap.set("n", "<leader>li", require("jdtls").organize_imports, { desc = "Java Organize Imports" })
-vim.keymap.set("n", "<leader>lv", require("jdtls").extract_variable, { desc = "Java Extract Variable" })
-vim.keymap.set("n", "<leader>lc", require("jdtls").extract_constant, { desc = "Java Extract Constant" })
+local map = vim.keymap.set
+map("n", "<leader>li", require("jdtls").organize_imports, { desc = "Java Organize Imports" })
+map("n", "<leader>lv", require("jdtls").extract_variable, { desc = "Java Extract Variable" })
+map("n", "<leader>lc", require("jdtls").extract_constant, { desc = "Java Extract Constant" })
 
-vim.keymap.set("n", "<leader>lt", require("jdtls").test_nearest_method, { desc = "Java Test Method" })
-vim.keymap.set("n", "<leader>lT", require("jdtls").test_class, { desc = "Java Test Class" })
+map("n", "<leader>lt", require("jdtls").test_nearest_method, { desc = "Java Test Method" })
+map("n", "<leader>lT", require("jdtls").test_class, { desc = "Java Test Class" })
 
-vim.keymap.set("n", "<leader>lu", "<Cmd> JdtUpdateConfig<CR>", { silent = true, desc = "Java Update Config" })
-vim.keymap.set("n", "<leader>lr", "<Cmd> JdtRestart<CR>", { silent = true, desc = "Java Restart JDTLS" })
+map("n", "<leader>lu", "<Cmd> JdtUpdateConfig<CR>", { silent = true, desc = "Java Update Config" })
+map("n", "<leader>lr", "<Cmd> JdtRestart<CR>", { silent = true, desc = "Java Restart JDTLS" })
