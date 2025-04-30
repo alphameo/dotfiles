4cebf6749921106d015b91f46b6b256d2abdff69
alphameo <pavel.2014.play@gmail.com>
2025-04-30 13:24:44 +0300

    fix(nvim): rm duplicated sesction from lualine

--- dot_config/nvim/lua/plugins/lualine.lua
+++ dot_config/nvim/lua/plugins/lualine.lua
@@ -59,11 +59,6 @@ return {
             cond = require("noice").api.status.command.has,
             color = { fg = "#ff9e64" },
           },
-          {
-            require("noice").api.status.mode.get,
-            cond = require("noice").api.status.mode.has,
-            color = { fg = "#ff9e64" },
-          },
           diagnostics,
           diff,
           { "encoding", cond = hide_in_width },
