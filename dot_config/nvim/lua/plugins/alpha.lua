72b148efa254a7bee85e2671c1efe4ee7ed164bf
alphameo <pavel.2014.play@gmail.com>
2025-04-30 13:26:23 +0300

    refactor(nvim): new config for alpha

--- dot_config/nvim/lua/plugins/alpha.lua
+++ dot_config/nvim/lua/plugins/alpha.lua
@@ -1,15 +1,10 @@
 return {
   "goolord/alpha-nvim",
-  dependencies = {
-    "nvim-tree/nvim-web-devicons",
-  },
   lazy = true,
   event = "VimEnter",
-
   config = function()
-    local alpha = require "alpha"
-    local dashboard = require "alpha.themes.startify"
-
+    -- local dashboard = require "alpha.themes.startify"
+    local dashboard = require "alpha.themes.dashboard"
     dashboard.section.header.val = {
       [[                                                     ]],
       [[  ███▄    █ ▓█████  ▒█████   ██▒   █▓ ██▓ ███▄ ▄███▓ ]],
@@ -25,6 +20,14 @@ return {
       [[                                                     ]],
     }
 
-    alpha.setup(dashboard.opts)
+    dashboard.section.buttons.val = {
+      dashboard.button("n", "   New file", ":ene<CR>"),
+      dashboard.button("f", "   Find file", ":Telescope find_files<CR>"),
+      dashboard.button("r", "   Recent", ":Telescope oldfiles<CR>"),
+      dashboard.button("s", "   Settings", ":e $MYVIMRC | :cd %:p:h | pwd<CR>"),
+      dashboard.button("q", "󰅙   Quit NVIM", ":qa<CR>"),
+    }
+
+    require("alpha").setup(dashboard.opts)
   end,
 }
