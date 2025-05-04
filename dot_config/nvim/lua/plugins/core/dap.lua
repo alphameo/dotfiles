51f48402645094f2679d3dc931058c8d8edf77dd
alphameo <pavel.2014.play@gmail.com>
2025-05-04 10:33:10 +0300

    refactor(nvim): dap cfgs

--- dot_config/nvim/lua/plugins/core/dap.lua
+++ dot_config/nvim/lua/plugins/core/dap.lua
@@ -53,30 +53,18 @@ return {
       type = "executable",
       command = "codelldb",
       -- detached = false, -- on windows you may have to uncomment this:
-      -- type = "server",
-      port = "${port}",
-      executable = {
-        command = "codelldb",
-        args = { "--port", "${port}" },
-      },
     }
     for _, lang in ipairs { "c", "cpp" } do
       dap.configurations[lang] = {
         {
+          name = "Launch file",
           type = "codelldb",
           request = "launch",
-          name = "Launch file",
           program = function()
             return vim.fn.input("Path to executable: ", vim.fn.getcwd() .. "/", "file")
           end,
           cwd = "${workspaceFolder}",
-        },
-        {
-          type = "codelldb",
-          request = "attach",
-          name = "Attach to process",
-          pid = require("dap.utils").pick_process,
-          cwd = "${workspaceFolder}",
+          stopOnEntry = false,
         },
       }
     end
@@ -184,5 +172,49 @@ return {
         },
       }
     end
+
+    -- NOTE: DAP GO
+    dap.adapters.delve = function(callback, config)
+      if config.mode == "remote" and config.request == "attach" then
+        callback {
+          type = "server",
+          host = config.host or "127.0.0.1",
+          port = config.port or "38697",
+        }
+      else
+        callback {
+          type = "server",
+          port = "${port}",
+          executable = {
+            command = "dlv",
+            args = { "dap", "-l", "127.0.0.1:${port}", "--log", "--log-output=dap" },
+            detached = vim.fn.has "win32" == 0,
+          },
+        }
+      end
+    end
+    dap.configurations.go = {
+      {
+        type = "delve",
+        name = "Debug",
+        request = "launch",
+        program = "${file}",
+      },
+      {
+        type = "delve",
+        name = "Debug test", -- configuration for debugging test files
+        request = "launch",
+        mode = "test",
+        program = "${file}",
+      },
+      -- works with go.mod packages and sub packages
+      {
+        type = "delve",
+        name = "Debug test (go.mod)",
+        request = "launch",
+        mode = "test",
+        program = "./${relativeFileDirname}",
+      },
+    }
   end,
 }
