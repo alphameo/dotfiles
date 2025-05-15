036f8e25b56a227d2664144c74a8ff92a963ac2d
alphameo <pavel.2014.play@gmail.com>
2025-05-15 14:04:15 +0300

    refactor(nvim): dap mappings via local veriable

--- dot_config/nvim/lua/plugins/core/dap.lua
+++ dot_config/nvim/lua/plugins/core/dap.lua
@@ -16,37 +16,38 @@ return {
     -- dap.listeners.before.event_terminated.dapui_config = dapui.close
     -- dap.listeners.before.event_exited.dapui_config = dapui.close
 
-    vim.keymap.set("n", "<leader>db", dap.toggle_breakpoint, { desc = "Debug () Toggle Breakpoint" })
-    vim.keymap.set("n", "<leader>dB", function()
+    local map = vim.keymap.set
+    map("n", "<leader>db", dap.toggle_breakpoint, { desc = "Debug () Toggle Breakpoint" })
+    map("n", "<leader>dB", function()
       dap.set_breakpoint(vim.fn.input "Breakpoint condition: ")
     end, { desc = "Debug () Set Conditional Breakpoint" })
 
-    vim.keymap.set("n", "<F29>", dapui.toggle, { desc = "Debug Toggle UI" })
-    vim.keymap.set("n", "<leader>dd", dapui.toggle, { desc = "Debug Interface" })
+    map("n", "<F29>", dapui.toggle, { desc = "Debug Toggle UI" })
+    map("n", "<leader>dd", dapui.toggle, { desc = "Debug Interface" })
 
-    vim.keymap.set("n", "<F5>", dap.continue, { desc = "Debug () Continue" })
-    vim.keymap.set("n", "<leader>dc", dap.continue, { desc = "Debug () Continue" })
-    vim.keymap.set("n", "<leader>da", function()
+    map("n", "<F5>", dap.continue, { desc = "Debug () Continue" })
+    map("n", "<leader>dc", dap.continue, { desc = "Debug () Continue" })
+    map("n", "<leader>da", function()
       dap.continue { before = get_args }
     end, { desc = "Debug (󱤵) Run with Args" })
 
-    vim.keymap.set("n", "<Leader>dl", dap.run_last, { desc = "Debug () Run Last" })
-    vim.keymap.set("n", "<leader>dp", dap.pause, { desc = "Debug () Pause" })
-    vim.keymap.set("n", "<leader>dt", dap.terminate, { desc = "Debug () Terminate" })
+    map("n", "<Leader>dl", dap.run_last, { desc = "Debug () Run Last" })
+    map("n", "<leader>dp", dap.pause, { desc = "Debug () Pause" })
+    map("n", "<leader>dt", dap.terminate, { desc = "Debug () Terminate" })
 
-    vim.keymap.set("n", "<F10>", dap.step_over, { desc = "Debug () Step Over" })
-    vim.keymap.set("n", "<leader>dO", dap.step_over, { desc = "Debug ()  Step Over" })
-    vim.keymap.set("n", "<F11>", dap.step_into, { desc = "Debug () Step Into" })
-    vim.keymap.set("n", "<leader>di", dap.step_into, { desc = "Debug () Step Into" })
-    vim.keymap.set("n", "<F23>", dap.step_out, { desc = "Debug () Step Out" })
-    vim.keymap.set("n", "<leader>do", dap.step_out, { desc = "Debug () Step Out" })
+    map("n", "<F10>", dap.step_over, { desc = "Debug () Step Over" })
+    map("n", "<leader>dO", dap.step_over, { desc = "Debug ()  Step Over" })
+    map("n", "<F11>", dap.step_into, { desc = "Debug () Step Into" })
+    map("n", "<leader>di", dap.step_into, { desc = "Debug () Step Into" })
+    map("n", "<F23>", dap.step_out, { desc = "Debug () Step Out" })
+    map("n", "<leader>do", dap.step_out, { desc = "Debug () Step Out" })
 
-    vim.keymap.set("n", "<leader>dj", dap.down, { desc = "Debug Down" })
-    vim.keymap.set("n", "<leader>dk", dap.up, { desc = "Debug Up" })
+    map("n", "<leader>dj", dap.down, { desc = "Debug Down" })
+    map("n", "<leader>dk", dap.up, { desc = "Debug Up" })
 
-    -- vim.keymap.set("n", "<leader>dr", dap.repl.toggle, { desc = "Debug Toggle REPL" })
-    -- vim.keymap.set("n", "<leader>ds", dap.session, { desc = "Debug Session" })
-    -- vim.keymap.set("n", "<leader>dw", require("dap.ui.widgets").hover, { desc = "Widgets" })
+    -- map("n", "<leader>dr", dap.repl.toggle, { desc = "Debug Toggle REPL" })
+    -- map("n", "<leader>ds", dap.session, { desc = "Debug Session" })
+    -- map("n", "<leader>dw", require("dap.ui.widgets").hover, { desc = "Widgets" })
 
     -- INFO: DAP CCPP
     dap.adapters.codelldb = {
