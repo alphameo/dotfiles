7e2e7ee53dca7593fb0db6510fe13deccc21cbaa
alphameo <pavel.2014.play@gmail.com>
2025-05-29 22:37:50 +0300

    refactor(nvim): rm git-blame and git-fugitive, replace it with git-signs

--- dot_config/nvim/lua/plugins/git-signs.lua
+++ dot_config/nvim/lua/plugins/git-signs.lua
@@ -2,20 +2,34 @@ return {
   "lewis6991/gitsigns.nvim",
   lazy = true,
   event = { "BufReadPost", "BufWritePost", "BufNewFile" },
-  opts = {
-    signs = {
-      add = { text = "▎" },
-      change = { text = "▎" },
-      delete = { text = "" },
-      topdelete = { text = "" },
-      changedelete = { text = "▎" },
-    },
-    signs_staged = {
-      add = { text = "▎" },
-      change = { text = "▎" },
-      delete = { text = "" },
-      topdelete = { text = "" },
-      changedelete = { text = "▎" },
-    },
-  },
+  config = function()
+    local map = vim.keymap.set
+    require("gitsigns").setup {
+      signs = {
+        add = { text = "▎" },
+        change = { text = "▎" },
+        delete = { text = "" },
+        topdelete = { text = "" },
+        changedelete = { text = "▎" },
+      },
+      signs_staged = {
+        add = { text = "▎" },
+        change = { text = "▎" },
+        delete = { text = "" },
+        topdelete = { text = "" },
+        changedelete = { text = "▎" },
+      },
+      current_line_blame = true,
+      word_diff = true,
+    }
+
+    map("n", "<leader>gb", ":Gitsigns blame<CR>", { silent = true, desc = "Git Blame" })
+    map("n", "<leader>gB", ":Gitsigns toggle_current_line_blame<CR>", { silent = true, desc = "Git Blame Line" })
+    map("n", "<leader>gh", ":Gitsigns preview_hunk_inline<CR>", { silent = true, desc = "Git Hunk Preview" })
+    map("n", "<leader>gw", ":Gitsigns toggle_word_diff<CR>", { silent = true, desc = "Git Word Diff" })
+    map("n", "]h", ":Gitsigns nav_hunk next<CR>", { silent = true, desc = "Git Hunk Next" })
+    map("n", "[h", ":Gitsigns nav_hunk prev<CR>", { silent = true, desc = "Git Hunk Prev" })
+    map("n", "<leader>ga", ":Gitsigns stage_hunk<CR>", { silent = true, desc = "Git Toggle Stage Hunk" })
+    map("n", "<leader>gr", ":Gitsigns reset_hunk<CR>", { silent = true, desc = "Git Hunk Reset" })
+  end,
 }
