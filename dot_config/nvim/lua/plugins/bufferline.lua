4ab254f3c3e223117e3380f316cb20fc7b991cbb
alphameo <pavel.2014.play@gmail.com>
2025-06-01 19:48:02 +0300

    refactor(nvim): rm unused param from func in bufferline

--- dot_config/nvim/lua/plugins/bufferline.lua
+++ dot_config/nvim/lua/plugins/bufferline.lua
@@ -18,7 +18,7 @@ return {
           style = "none", -- Options: 'icon', 'underline', 'none'
         },
         diagnostics = "nvim_lsp",
-        diagnostics_indicator = function(count, level, diagnostics_dict, context)
+        diagnostics_indicator = function(count, level)
           local icon = level:match "error" and " " or " "
           return " " .. icon .. count
         end,
