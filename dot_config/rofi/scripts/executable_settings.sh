bbbfee9d242bf32fe9d7b64461065c1f9f6a01d3
alphameo <pavel.2014.play@gmail.com>
2025-06-12 11:25:32 +0300

    feat(rofi): settings menu

---
+++ dot_config/rofi/scripts/executable_settings.sh
@@ -0,0 +1,74 @@
+#!/usr/bin/bash
+
+open_config() {
+    local module="$1"
+    if [[ ${#module} != 0 ]]; then
+        coproc ($TERMINAL -d $XDG_CONFIG_HOME/$module $EDITOR)
+        exit 0
+    fi
+}
+
+execute() {
+    coproc (eval "$1")
+    exit 0
+}
+
+function launch_cli_app {
+    coproc ($TERMINAL "$1")
+    exit 0
+}
+
+function open_in_term {
+    coproc($TERMINAL --hold "$1")
+    exit 0
+}
+
+function run {
+    INP="$@"
+    ROFI=" rofi"
+    WAYBAR=" waybar"
+    HYPR=" hypr"
+    DUNST="󰵚 dunst"
+    NETWORK=" wifi"
+    BLUETOOTH="󰂯 bluetooth"
+    TOP="󰙭 monitor"
+    SOUND=" sound"
+    INFO=" info"
+
+    if [[ $INP == $ROFI ]]; then
+        open_config "rofi"
+    elif [[ $INP == $WAYBAR ]]; then
+        open_config "waybar"
+    elif [[ $INP == $HYPR ]]; then
+        open_config "hypr"
+    elif [[ $INP == $DUNST ]]; then
+        open_config "dunst"
+    elif [[ $INP == $NETWORK ]]; then
+        execute "networkmanager_dmenu"
+    elif [[ $INP == $BLUETOOTH ]]; then
+        execute "rofi-bluetooth"
+    elif [[ $INP == $SOUND ]]; then
+        execute "pavucontrol"
+    elif [[ $INP == $TOP ]]; then
+        launch_cli_app $SYSMONITOR
+    elif [[ $INP == $INFO ]]; then
+        open_in_term "fastfetch"
+    else
+        echo $ROFI
+        echo $WAYBAR
+        echo $HYPR
+        echo $DUNST
+        echo $NETWORK
+        echo $BLUETOOTH
+        echo $TOP
+        echo $SOUND
+        echo $INFO
+        exit 0
+    fi
+}
+
+if [[ $1 == "open" ]]; then
+    rofi -show "   " -modes "   :~/.config/rofi/scripts/settings.sh"
+else
+    run $@
+fi
