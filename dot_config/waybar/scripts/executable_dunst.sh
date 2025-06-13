dfb4dd75fdd069d2bec01be405a2a51fb81517fb
alphameo <pavel.2014.play@gmail.com>
2025-06-14 00:29:20 +0300

    refactor(waybar): reformat dunst script

--- dot_config/waybar/scripts/executable_dunst.sh
+++ dot_config/waybar/scripts/executable_dunst.sh
@@ -1,7 +1,13 @@
 #!/bin/bash
 
 COUNT=$(dunstctl count waiting)
-ENABLED="<span color='#f9e2af'>󰂚</span>"
-DISABLED="<span color='#f38ba8'>󱏧</span>"
-if [ $COUNT != 0 ]; then DISABLED="$DISABLED $COUNT"; fi
-if dunstctl is-paused | grep -q "false" ; then echo $ENABLED; else echo $DISABLED; fi
+ENABLED="<span color='#f9e2af'>󰂚</span> "
+DISABLED="<span color='#f38ba8'>󱏧</span> "
+if [ $COUNT != 0 ]; then
+    DISABLED="$DISABLED $COUNT"
+fi
+if dunstctl is-paused | grep -q "false"; then
+    echo $ENABLED
+else
+    echo $DISABLED
+fi
