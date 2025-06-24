01efadbc721bebda5871567f34e3ede7f6c6b89a
alphameo <pavel.2014.play@gmail.com>
2025-06-24 14:33:23 +0300

    refactor(treesitter): bump to latest and adapt cfg

--- dot_config/nvim/lua/plugins/core/treesitter.lua
+++ dot_config/nvim/lua/plugins/core/treesitter.lua
@@ -1,61 +1,84 @@
 return {
   "nvim-treesitter/nvim-treesitter",
-  version = false, -- last release doesn't work on windows
-  lazy = true,
-  event = { "BufReadPost", "BufNewFile", "VeryLazy" },
-  cmd = { "TSUpdate", "TSInstall", "TSBufEnable", "TSBufDisable" },
+  lazy = false,
+  branch = "main",
   build = ":TSUpdate",
   config = function()
-    local config = require "nvim-treesitter.configs"
-    config.setup {
-      ensure_installed = {
-        "lua",
-        "java",
-        "c",
-        "cpp",
-        "kotlin",
-        "python",
-        "go",
-        "gomod",
-        "gosum",
-        "gowork",
-        "rust",
-        "ron",
-        "make",
-        "cmake",
-        "html",
-        "css",
-        "scss",
-        "javascript",
-        "typescript",
-        "php",
-        "sql",
-        "luadoc",
-        "vim",
-        "vimdoc",
-        "regex",
-        "printf",
-        "markdown",
-        "markdown_inline",
-        "bash",
-        "toml",
-        "yaml",
-        "xml",
-        "json5",
-        "diff",
-        "gitignore",
-        "bibtex",
-        "dockerfile",
-        "hyprlang",
-        "yuck"
-      },
+    local langs = {
+      "lua",
+      "java",
+      "c",
+      "cpp",
+      "kotlin",
+      "python",
+      "go",
+      "gomod",
+      "gosum",
+      "gowork",
+      "rust",
+      "ron",
+      "make",
+      "cmake",
+      "html",
+      "css",
+      "scss",
+      "javascript",
+      "typescript",
+      "php",
+      "sql",
+      "luadoc",
+      "vim",
+      "vimdoc",
+      "regex",
+      "printf",
+      "markdown",
+      "markdown_inline",
+      "bash",
+      "toml",
+      "yaml",
+      "xml",
+      "json5",
+      "diff",
+      "gitignore",
+      "bibtex",
+      "dockerfile",
+      "hyprlang",
+      "yuck",
+    }
+
+    require("nvim-treesitter").setup {
+      ensure_installed = langs,
+      sync_install = false,
+      auto_install = false,
       highlight = {
         enable = true,
         use_languagetree = true,
+        additional_vim_regex_highlighting = false,
       },
       indent = {
         enable = true,
       },
+      incremental_selection = {
+        enable = true,
+        keymaps = {
+          init_selection = "gnn",
+          node_incremental = "grn",
+          scope_incremental = "grc",
+          node_decremental = "grm",
+        },
+      },
     }
+
+    require("nvim-treesitter").install(langs)
+
+    vim.api.nvim_create_autocmd("FileType", {
+      pattern = langs,
+      callback = function()
+        vim.treesitter.start()
+      end,
+    })
+
+    local map = vim.keymap.set
+    map("n", "<leader>ct", ":InspectTree<CR>", { silent = true, desc = "Code Inspec Tree" })
   end,
 }
