97a0320e48ef3079fb1656b84008eea13fb84626
alphameo <pavel.2014.play@gmail.com>
2025-06-28 15:55:44 +0300

    feat(nvim-minimal): add cfg

---
+++ dot_config/nvim-minimal/init.lua
@@ -0,0 +1,51 @@
+-- Bootstrap lazy.nvim
+local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
+if not (vim.uv or vim.loop).fs_stat(lazypath) then
+	local lazyrepo = "https://github.com/folke/lazy.nvim.git"
+	local out = vim.fn.system({
+		"git",
+		"clone",
+		"--filter=blob:none",
+		"--branch=stable",
+		lazyrepo,
+		lazypath,
+	})
+	if vim.v.shell_error ~= 0 then
+		vim.api.nvim_echo({
+			{ "Failed to clone lazy.nvim:\n", "ErrorMsg" },
+			{ out, "WarningMsg" },
+			{ "\nPress any key to exit..." },
+		}, true, {})
+		vim.fn.getchar()
+		os.exit(1)
+	end
+end
+vim.opt.rtp:prepend(lazypath)
+
+-- Make sure to setup `mapleader` and `maplocalleader` before
+-- loading lazy.nvim so that mappings are correct.
+-- This is also a good place to setup other settings (vim.opt)
+vim.g.mapleader = " "
+vim.g.maplocalleader = " "
+
+require("lazy").setup({
+	"nvim-treesitter/nvim-treesitter",
+	build = function()
+		local ts_update = require("nvim-treesitter.install").update({
+			with_sync = true,
+		})
+		ts_update()
+	end,
+	config = function()
+		local ts_config = require("nvim-treesitter.configs")
+
+		ts_config.setup({
+			ensure_installed = "all",
+		})
+	end,
+}, {
+	change_detection = {
+		enabled = false,
+		notify = false,
+	},
+})
