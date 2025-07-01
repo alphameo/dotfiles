07a73433a532fecb0680bc2193dcd73655108ba0
alphameo <pavel.2014.play@gmail.com>
2025-07-01 14:09:33 +0300

    refactor(nvim): add support for line-formatting

--- dot_config/nvim/lua/plugins/core/conform.lua
+++ dot_config/nvim/lua/plugins/core/conform.lua
@@ -51,19 +51,28 @@ return {
         },
       },
     }
-    local fmt = function()
-      conform.format {
-        lsp_fallback = true,
-        async = false,
-        timeout_ms = 1000,
-      }
-    end
+
+    vim.api.nvim_create_user_command("Format", function(args)
+      local range = nil
+      local fmt_range
+      if args.count ~= -1 then
+        fmt_range = args.line1 .. "-" .. args.line2
+        local end_line = vim.api.nvim_buf_get_lines(0, args.line2 - 1, args.line2, true)[1]
+        range = {
+          start = { args.line1, 0 },
+          ["end"] = { args.line2, end_line:len() },
+        }
+      else
+        fmt_range = "whole file"
+      end
+      conform.format { async = true, lsp_format = "fallback", range = range }
+      vim.notify("Lines for formatting: " .. fmt_range)
+    end, { range = true })
 
     vim.o.formatexpr = "v:lua.require'conform'.formatexpr()"
 
     local map = vim.keymap.set
-    map({ "n", "v", "i" }, "<C-S-i>", fmt, { desc = "Code Format File" })
-    map({ "n", "v" }, "<leader>cf", fmt, { desc = "Code Format File" })
-    map({ "n", "v" }, "<leader>cF", "gw", { desc = "Code Format Expr" })
+    map({ "n", "v" }, "<C-S-i>", ":Format<CR>", { silent = true, desc = "Code Format" })
+    map({ "n", "v" }, "<leader>cf", ":Format<CR>", { silent = true, desc = "Code Format" })
   end,
 }
