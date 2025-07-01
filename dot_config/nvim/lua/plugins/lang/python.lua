5fcf9dc74a5c132a6adeddd8edd98fd720b8cf6e
alphameo <pavel.2014.play@gmail.com>
2025-07-01 19:59:47 +0300

    refactor(nvim): mv ipynb usercmd to python plugins file

--- dot_config/nvim/lua/plugins/lang/python.lua
+++ dot_config/nvim/lua/plugins/lang/python.lua
@@ -1,3 +1,55 @@
+local default_notebook = [[
+  {
+    "cells": [
+     {
+      "cell_type": "markdown",
+      "metadata": {},
+      "source": [
+        ""
+      ]
+     }
+    ],
+    "metadata": {
+     "kernelspec": {
+      "display_name": "Python 3",
+      "language": "python",
+      "name": "python3"
+     },
+     "language_info": {
+      "codemirror_mode": {
+        "name": "ipython"
+      },
+      "file_extension": ".py",
+      "mimetype": "text/x-python",
+      "name": "python",
+      "nbconvert_exporter": "python",
+      "pygments_lexer": "ipython3"
+     }
+    },
+    "nbformat": 4,
+    "nbformat_minor": 5
+  }
+]]
+
+local function new_notebook(filename)
+  local path = filename .. ".ipynb"
+  local file = io.open(path, "w")
+  if file then
+    file:write(default_notebook)
+    file:close()
+    vim.cmd("edit " .. path)
+  else
+    print "Error: Could not open new notebook file for writing."
+  end
+end
+
+vim.api.nvim_create_user_command("NewIPYNB", function(opts)
+  new_notebook(opts.args)
+end, {
+  nargs = 1,
+  complete = "file",
+})
+
 return {
   {
     "mfussenegger/nvim-dap-python",
@@ -8,7 +60,6 @@ return {
     "benlubas/molten-nvim",
     lazy = true,
     ft = { "python", "markdown", "json" },
-    fr = { "ipynb" },
     version = "^1.0.0",
     dependencies = { "3rd/image.nvim" },
     build = ":UpdateRemotePlugins",
@@ -73,22 +124,22 @@ return {
     },
     lazy = true,
     ft = { "quarto", "ipynb", "markdown" },
-    opts = {
-      lspFeatures = {
-        languages = { "python" },
-        chunks = "all",
-        diagnostics = {
-          enabled = true,
-          triggers = { "BufWritePost" },
-        },
-        completion = { enabled = true },
-      },
-      codeRunner = {
-        enabled = true,
-        default_method = "molten",
-      },
-    },
     config = function()
+      require("quarto").setup {
+        lspFeatures = {
+          languages = { "python" },
+          chunks = "all",
+          diagnostics = {
+            enabled = true,
+            triggers = { "BufWritePost" },
+          },
+          completion = { enabled = true },
+        },
+        codeRunner = {
+          enabled = true,
+          default_method = "molten",
+        },
+      }
       local map = vim.keymap.set
       map("n", "<leader>lq", ":QuartoActivate<CR>", { silent = true, desc = "Quarto Initialize" })
       map("n", "<leader>lP", ":QuartoPreview<CR>", { silent = true, desc = "Quarto Preview" })
