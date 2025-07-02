a09d6bd60d5c2b15d0c2cd5fc445f8a74253acfe
alphameo <pavel.2014.play@gmail.com>
2025-07-02 15:03:18 +0300

    refactor(nvim): extract treesitter-context for lazy loading

--- dot_config/nvim/lua/plugins/core/treesitter.lua
+++ dot_config/nvim/lua/plugins/core/treesitter.lua
@@ -1,102 +1,110 @@
 return {
-  "nvim-treesitter/nvim-treesitter",
-  dependencies = {
-    -- TODO: Enable when master branch become default
-    -- { "nvim-treesitter/nvim-treesitter-textobjects", branch = "main" },
-    {
-      "nvim-treesitter/nvim-treesitter-context",
-      opts = { enable = true, mode = "topline", line_numbers = true },
+  {
+    "nvim-treesitter/nvim-treesitter",
+    dependencies = {
+      -- TODO: Enable when master branch become default
+      -- { "nvim-treesitter/nvim-treesitter-textobjects", branch = "main" },
+    },
+    lazy = false,
+    branch = "main",
+    build = ":TSUpdate",
+    config = function()
+      local langs = {
+        -- LANGUAGES
+        "bash",
+        "c",
+        "cpp",
+        "cmake",
+        "go",
+        "gomod",
+        "gosum",
+        "gowork",
+        "java",
+        "javascript",
+        "kotlin",
+        "lua",
+        "luadoc",
+        "make",
+        "python",
+        "php",
+        "ron",
+        "rust",
+        "scheme",
+        "sql",
+        "typescript",
+        "vim",
+        "vimdoc",
+        -- MARKUP
+        "bibtex",
+        "css",
+        "html",
+        "markdown",
+        "markdown_inline",
+        "mermaid",
+        "scss",
+        "xml",
+        -- DATA
+        "json",
+        "jsonc",
+        "json5",
+        -- TOOLS
+        "diff",
+        "dockerfile",
+        "gitignore",
+        "git_config",
+        "git_rebase",
+        "gitcommit",
+        "printf",
+        "regex",
+        "query",
+        -- CONFIGURATIONS
+        "dot",
+        "hyprlang",
+        "ini",
+        "toml",
+        "rasi",
+        "ssh_config",
+        "yuck",
+        "yaml",
+      }
+
+      require("nvim-treesitter").setup {
+        ensure_installed = langs,
+        sync_install = false,
+        auto_install = false,
+        highlight = {
+          enable = true,
+          use_languagetree = true,
+          additional_vim_regex_highlighting = false,
+          disable = {},
+        },
+        indent = { enable = true },
+        -- textobjects = { select = { enable = true, lookahead = true } },
+      }
+
+      require("nvim-treesitter").install(langs)
+
+      vim.api.nvim_create_autocmd("FileType", {
+        pattern = langs,
+        callback = function()
+          vim.treesitter.start()
+          vim.wo.foldexpr = "v:lua.vim.treesitter.foldexpr()"
+          vim.bo.indentexpr = "v:lua.require'nvim-treesitter'.indentexpr()"
+        end,
+      })
+
+      local map = vim.keymap.set
+      map("n", "<leader>ct", ":InspectTree<CR>", { silent = true, desc = "Code Inspect Tree" })
+    end,
+  },
+  {
+    "nvim-treesitter/nvim-treesitter-context",
+    lazy = true,
+    event = "VeryLazy",
+    opts = {
+      enable = true,
+      mode = "topline",
+      line_numbers = true,
     },
   },
-  lazy = false,
-  branch = "main",
-  build = ":TSUpdate",
-  config = function()
-    local langs = {
-      -- LANGUAGES
-      "bash",
-      "c",
-      "cpp",
-      "cmake",
-      "go",
-      "gomod",
-      "gosum",
-      "gowork",
-      "java",
-      "javascript",
-      "kotlin",
-      "lua",
-      "luadoc",
-      "make",
-      "python",
-      "php",
-      "ron",
-      "rust",
-      "scheme",
-      "sql",
-      "typescript",
-      "vim",
-      "vimdoc",
-      -- MARKUP
-      "bibtex",
-      "css",
-      "html",
-      "markdown",
-      "markdown_inline",
-      "mermaid",
-      "scss",
-      "xml",
-      -- DATA
-      "json",
-      "jsonc",
-      "json5",
-      -- TOOLS
-      "diff",
-      "dockerfile",
-      "gitignore",
-      "git_config",
-      "git_rebase",
-      "gitcommit",
-      "printf",
-      "regex",
-      "query",
-      -- CONFIGURATIONS
-      "dot",
-      "hyprlang",
-      "ini",
-      "toml",
-      "rasi",
-      "ssh_config",
-      "yuck",
-      "yaml",
-    }
-
-    require("nvim-treesitter").setup {
-      ensure_installed = langs,
-      sync_install = false,
-      auto_install = false,
-      highlight = {
-        enable = true,
-        use_languagetree = true,
-        additional_vim_regex_highlighting = false,
-        disable = {},
-      },
-      indent = { enable = true },
-      textobjects = { select = { enable = true, lookahead = true } },
-    }
-
-    require("nvim-treesitter").install(langs)
-
-    vim.api.nvim_create_autocmd("FileType", {
-      pattern = langs,
-      callback = function()
-        vim.treesitter.start()
-        vim.wo.foldexpr = "v:lua.vim.treesitter.foldexpr()"
-        vim.bo.indentexpr = "v:lua.require'nvim-treesitter'.indentexpr()"
-      end,
-    })
-
-    local map = vim.keymap.set
-    map("n", "<leader>ct", ":InspectTree<CR>", { silent = true, desc = "Code Inspect Tree" })
-  end,
 }
