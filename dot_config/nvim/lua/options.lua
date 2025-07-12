147a1a49fa0f5f99dab08f81fb727537ad9b3c17
alphameo <pavel.2014.play@gmail.com>
2025-07-12 23:17:05 +0300

    refactor(nvim): reorganize options

--- dot_config/nvim/lua/options.lua
+++ dot_config/nvim/lua/options.lua
@@ -1,58 +1,86 @@
--- vim.cmd "source ~/.config/.vimrc"
 local opt = vim.opt
 local o = vim.o
-local g = vim.g
 
-o.number = true -- Make line numbers default (default: false)
-opt.nu = true
-opt.relativenumber = true -- Set relative numbered lines (default: false)
+-----------
+-- BASIC --
+-----------
+opt.number = true -- Line numbers (false)
+opt.relativenumber = true -- Set relative numbered lines (false)
+opt.cursorline = true -- Highlight current line (false)
+opt.cursorcolumn = false -- Highlight current column (false)
+opt.wrap = false -- Display lines as one long line (true)
+opt.linebreak = true -- Companion to wrap, don't split words (false)
+opt.scrolloff = 10 -- Keep n lines above/below cursor (0)
+opt.sidescrolloff = 8 -- Keep n columns left/right of cursor (0)
 
-opt.tabstop = 4 -- Insert n spaces for a tab (default: 8)
-opt.softtabstop = 4 -- Number of spaces that a tab counts for while performing editing operations (default: 0)
-opt.shiftwidth = 4 -- The number of spaces inserted for each indentation (default: 8)
-opt.expandtab = true -- Convert tabs to spaces (default: false)
-opt.smartindent = true
-opt.autoindent = true -- Copy indent from current line when starting new one (default: true)
-opt.breakindent = true -- Enable break indent (default: false)
+-----------------
+-- INDENTATION --
+-----------------
+opt.tabstop = 4 -- Tab width (8)
+opt.shiftwidth = 4 -- Indent width (8)
+opt.softtabstop = 4 -- Number of spaces that tab counts (0)
+opt.expandtab = true -- Spaces instead of tabs (false)
+opt.smartindent = true -- Smart auto-indenting (false)
+opt.autoindent = true -- Copy indent from current line (true)
+opt.breakindent = true -- Keep wrapped line on same indent level (default: false)
 
-opt.wrap = false -- Display lines as one long line (default: true)
-opt.linebreak = true -- Companion to wrap, don't split words (default: false)
+------------
+-- SEARCH --
+------------
+opt.ignorecase = true -- Case-insensitive search (false)
+opt.smartcase = true -- Case sensitive if uppercase in search (false)
+opt.hlsearch = false -- Highlight search results (true)
+opt.incsearch = true -- Show matches as you type (false)
 
-opt.swapfile = false -- Creates a swapfile (default: true)
-opt.backup = false -- Creates a backup file (default: false)
-opt.undofile = true -- Save undo history (default: false)
-opt.undodir = os.getenv "HOME" .. "/.vim/undodir"
-
-opt.hlsearch = false -- Set highlight on search (default: true)
-opt.incsearch = true
-opt.ignorecase = true -- Case-insensitive searching UNLESS \C or capital in search (default: false)
-opt.smartcase = true -- Smart case (default: false)
-
-opt.termguicolors = true -- Set termguicolors to enable highlight groups (default: false)
-
-opt.scrolloff = 8 -- Minimal number of screen lines to keep above and below the cursor (default: 0)
-opt.sidescrolloff = 8 -- Minimal number of screen columns either sideof cursor if wrap is `false` (default: 0)
-opt.signcolumn = "yes" -- Keep signcolumn on by default (default: 'auto')
-opt.isfname:append "@-@"
-
-opt.updatetime = 50 -- Decrease update time (default: 4000)
-vim.opt.timeoutlen = 200 -- keypress delay
--- vim.opt.ttimeoutlen = 20 -- mode change delay
-
-opt.clipboard = "unnamedplus" -- Sync clipboard between OS and Neovim. (default: '')
-opt.mouse = "a" -- Enable mouse mode (default: '')
-
-opt.showtabline = 2 -- Always show tabs (default: 1)
+------------
+-- VISUAL --
+------------
+opt.termguicolors = true -- 24-bit colors (false)
+opt.signcolumn = "yes" -- Show signcolumn ("auto")
+opt.colorcolumn = "" -- Show column at "n" characters
 opt.conceallevel = 0 -- Visual-control chars (**, '') in md etc. (0 - visible, 1-faded(def), 2-hidden, 3-idk)
 
-opt.pumheight = 10 -- Pop up menu height (default: 0)
+-------------------
+-- FILE HANDLING --
+-------------------
+opt.backup = false -- Creates a backup file (false)
+opt.swapfile = false -- Creates a swapfile (true)
+opt.undofile = true -- Save undo history (false)
+opt.undodir = os.getenv "HOME" .. "/.vim/undodir"
+opt.updatetime = 300 -- Decrease update time (4000)
+opt.autoread = true -- Auto reload files changed outside vim (false)
+opt.autowrite = false -- Don't auto save (false)
+opt.encoding = "UTF-8" -- Set encoding
+opt.isfname:append "@-@" -- Allow @ for filenames
 
--- disable some default providers
-g.loaded_node_provider = 0
-g.loaded_python3_provider = 0
-g.loaded_perl_provider = 0
-g.loaded_ruby_provider = 0
+-----------
+-- INPUT --
+-----------
+opt.clipboard:append "unnamedplus" -- Use system clipboard ('')
+opt.timeoutlen = 200 -- Key timeout duration (500)
+opt.ttimeoutlen = 0 -- Key code timeout
+opt.mouse = "a" -- Enable mouse support (default: '')
 
+-----------
+-- SPELL --
+-----------
 o.spelllang = "ru_ru,en_us"
 o.spell = false
 
+-----------
+-- SPLIT --
+-----------
+opt.splitbelow = true -- Horizontal splits go below
+opt.splitright = true -- Vertical splits go right
+
+-----------
+-- OTHER --
+-----------
+vim.filetype.add {
+  pattern = {
+    [".*/.config/hypr/.*%.conf"] = "hyprlang",
+    [".*/.config/waybar/.*%.css"] = "gtkcss",
+    [".*/.config/rofi/.*%.css"] = "gtkcss",
+    [".*/.config/kitty/.*%.conf"] = "kitty",
+  },
+}
