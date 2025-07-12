3872b76bb14abf75cc63f5890a21a4e24f5f9090
alphameo <pavel.2014.play@gmail.com>
2025-07-12 23:15:55 +0300

    refactor(nvim): mv mason's opt with binaries path

--- dot_config/nvim/lua/plugins/core/mason.lua
+++ dot_config/nvim/lua/plugins/core/mason.lua
@@ -112,5 +112,10 @@ return {
         -- "yamllint", -- yaml lint
       },
     }
+    -- add binaries installed by mason.nvim to path
+    local is_windows = vim.fn.has "win32" ~= 0
+    local sep = is_windows and "\\" or "/"
+    local delim = is_windows and ";" or ":"
+    vim.env.PATH = table.concat({ vim.fn.stdpath "data", "mason", "bin" }, sep) .. delim .. vim.env.PATH
   end,
 }
