50c8924e9dfff4c6661ccbf5f14846b23f8264fc
alphameo <pavel.2014.play@gmail.com>
2025-07-29 15:32:05 +0300

    fix(nvim): colorizer config

--- dot_config/nvim/lua/plugins/colorizer.lua
+++ dot_config/nvim/lua/plugins/colorizer.lua
@@ -4,26 +4,13 @@ return {
   event = { "BufReadPost", "BufNewFile" },
   config = function()
     require("colorizer").setup {
-      DEFAULT_OPTIONS = {
-        RGB = true, -- #RGB hex codes
-        RRGGBB = true, -- #RRGGBB hex codes
-        names = true, -- "Name" codes like Blue
-        RRGGBBAA = true, -- #RRGGBBAA hex codes
-        rgb_fn = true, -- CSS rgb() and rgba() functions
-        hsl_fn = true, -- CSS hsl() and hsla() functions
-        css = true, -- Enable all CSS features: rgb_fn, hsl_fn, names, RGB, RRGGBB
-        css_fn = true, -- Enable all CSS *functions*: rgb_fn, hsl_fn
-        mode = "background",
+      "*",
+      css = {
+        RRGGBBAA = true,
+        rgb_fn = true,
+        hsl_fn = true,
       },
     }
-
-    vim.api.nvim_create_autocmd({ "BufReadPost", "BufNewFile" }, {
-      group = vim.api.nvim_create_augroup("InitColorizer", {}),
-      desc = "Enables Colorizer on Open File",
-      pattern = "*",
-      command = 'ColorizerToggle',
-    })
-
     vim.keymap.set("n", "<leader>aC", ":ColorizerToggle<CR>", { silent = true, desc = "Toggle Colorizer" })
   end,
 }
