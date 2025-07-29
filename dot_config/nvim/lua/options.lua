f76cdb752f4c11598d14483f2c724bbba3c9da6f
alphameo <pavel.2014.play@gmail.com>
2025-07-29 17:45:28 +0300

    refactor(nvim): mv Path extension by mason binaries into options

--- dot_config/nvim/lua/options.lua
+++ dot_config/nvim/lua/options.lua
@@ -58,3 +58,8 @@ vim.filetype.add {
     [".*/.config/kitty/.*%.conf"] = "ini",
   },
 }
+-- add binaries installed by mason.nvim to path
+local is_windows = vim.fn.has "win32" ~= 0
+local sep = is_windows and "\\" or "/"
+local delim = is_windows and ";" or ":"
+vim.env.PATH = table.concat({ vim.fn.stdpath "data", "mason", "bin" }, sep) .. delim .. vim.env.PATH
--- dot_config/nvim/lua/plugins/core/mason.lua
+++ dot_config/nvim/lua/plugins/core/mason.lua
@@ -112,10 +112,5 @@ return {
         -- "yamllint", -- yaml lint
       },
     }
-    -- add binaries installed by mason.nvim to path
-    local is_windows = vim.fn.has "win32" ~= 0
-    local sep = is_windows and "\\" or "/"
-    local delim = is_windows and ";" or ":"
-    vim.env.PATH = table.concat({ vim.fn.stdpath "data", "mason", "bin" }, sep) .. delim .. vim.env.PATH
   end,
 }
