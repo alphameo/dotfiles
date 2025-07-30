ba13449e25e3601cf397856e0a2e96b047547d40
alphameo <pavel.2014.play@gmail.com>
2025-07-30 15:25:05 +0300

    refactor(nvim): disable list on startup in options

--- dot_config/nvim/lua/options.lua
+++ dot_config/nvim/lua/options.lua
@@ -67,7 +67,7 @@ vim.o.pumheight = 10 -- Make popup menu smaller
 vim.o.winblend = 10 -- Make floating windows slightly transparent
 
 vim.o.listchars = "tab:> ,extends:…,precedes:…,nbsp:␣" -- Define which helper symbols to show
-vim.o.list = true -- Show some helper symbols
+vim.o.list = false -- Show some helper symbols
 
 if vim.fn.exists "syntax_on" ~= 1 then
   vim.cmd "syntax enable" -- Enable syntax highlighting
