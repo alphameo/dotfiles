bdf94f8e87ab11ed014a837716508f604987c4f6
alphameo <pavel.2014.play@gmail.com>
2025-07-30 13:14:37 +0300

    refactor(nvim): reorder opts

--- dot_config/nvim/lua/options.lua
+++ dot_config/nvim/lua/options.lua
@@ -1,76 +1,91 @@
 -- General
-vim.cmd "filetype plugin indent on"
+vim.o.undofile = true -- Save undo history
+vim.o.undodir = os.getenv "HOME" .. "/.vim/undodir"
 
--- Basic
-vim.g.have_nerd_font = true
-vim.o.number = true -- Line numbers (false)
-vim.o.relativenumber = true -- Set relative numbered lines (false)
-vim.o.cursorline = true -- Highlight current line (false)
-vim.o.wrap = false -- Display lines as one long line (true)
-vim.o.linebreak = true -- Companion to wrap, don't split words (false)
-vim.o.scrolloff = 10 -- Keep n lines above/below cursor (0)
-vim.o.sidescrolloff = 8 -- Keep n columns left/right of cursor (0)
+vim.o.backup = false -- Don't store backup while overwriting the file
+vim.o.writebackup = false -- Don't store backup while overwriting the file
 
--- Indentation
-vim.o.tabstop = 4 -- Tab width (8)
-vim.o.shiftwidth = 4 -- Indent width (8)
-vim.o.softtabstop = 4 -- Number of spaces that tab counts (0)
-vim.o.expandtab = true -- Spaces instead of tabs (false)
-vim.o.smartindent = true -- Smart auto-indenting (false)
-vim.o.autoindent = true -- Copy indent from current line (true)
-vim.o.breakindent = true -- Keep wrapped line on same indent level (false)
+vim.o.mouse = "a" -- Enable mouse support
 
--- Search
-vim.o.ignorecase = true -- Case-insensitive search (false)
-vim.o.infercase = true -- Infer letter cases for a richer built-in keyword completion (false)
-vim.o.smartcase = true -- Case sensitive if uppercase in search (false)
-vim.o.hlsearch = false -- Highlight search results (true)
-vim.o.incsearch = true -- Show matches as you type (false)
+vim.schedule(function()
+  vim.o.clipboard = "unnamedplus" -- Use system clipboard
+end)
 
--- Visual
-vim.o.termguicolors = true -- 24-bit colors (false)
-vim.o.signcolumn = "yes" -- Show signcolumn ("auto")
-vim.o.fillchars = "eob: " -- Don't show `~` outside of buffer
-vim.o.colorcolumn = "" -- Show column at "n" characters
-vim.o.conceallevel = 0 -- Visual-control chars (**, '') in md etc. (0 - visible, 1-faded(def), 2-hidden, 3-idk)
+vim.o.timeoutlen = 200 -- Key timeout duration
+vim.o.ttimeoutlen = 10 -- Key code timeout
+
+vim.o.confirm = true -- Ask when did not save on exit
+
+vim.o.swapfile = false -- Creates a swapfile
+vim.o.updatetime = 250 -- Decrease update time
+vim.opt.isfname:append "@-@" -- Allow @ for filenames
+
+vim.o.spelllang = "ru_ru,en_us" -- List for spellchecks
+vim.o.spell = false -- Enable spellcheck
+
+vim.cmd "filetype plugin indent on" -- Load settings, mappings, indent per filetype
+
+-- Appearance
+vim.o.tabstop = 4 -- Tab width
+vim.o.shiftwidth = 4 -- Indent width
+vim.o.softtabstop = 4 -- Number of spaces that tab counts
+vim.o.expandtab = true -- Spaces instead of tabs
+vim.o.smartindent = true -- Smart auto-indenting
+vim.o.autoindent = true -- Copy indent from current line
+vim.o.breakindent = true -- Keep wrapped line on same indent level
+
+vim.o.cursorline = true -- Highlight current line
+
+vim.o.wrap = false -- Display lines as one long line
+vim.o.linebreak = true -- Companion to wrap, don't split words
+
+vim.o.number = true -- Line numbers
+vim.o.relativenumber = true -- Set relative numbered lines
+
+vim.o.scrolloff = 10 -- Keep n lines above/below cursor
+vim.o.sidescrolloff = 8 -- Keep n columns left/right of cursor
+
 vim.o.splitbelow = true -- Horizontal splits go below
 vim.o.splitright = true -- Vertical splits go right
 
--- File Handling
-vim.o.backup = false -- Don't store backup while overwriting the file
-vim.o.writebackup = false -- Don't store backup while overwriting the file
-vim.o.swapfile = false -- Creates a swapfile (true)
-vim.o.undofile = true -- Save undo history (false)
-vim.o.undodir = os.getenv "HOME" .. "/.vim/undodir"
-vim.o.updatetime = 250 -- Decrease update time (4000)
-vim.o.encoding = "UTF-8" -- Set encoding
-vim.opt.isfname:append "@-@" -- Allow @ for filenames
-vim.o.confirm = true -- Ask when did not save on exit
+vim.o.signcolumn = "yes" -- Show signcolumn
+vim.o.fillchars = "eob: " -- Don't show `~` outside of buffer
 
--- Input
-vim.schedule(function()
-  vim.o.clipboard = "unnamedplus"
-end) -- Use system clipboard ('')
-vim.o.timeoutlen = 200 -- Key timeout duration (500)
-vim.o.ttimeoutlen = 10 -- Key code timeout
-vim.o.mouse = "a" -- Enable mouse support (default: '')
+vim.o.termguicolors = true -- 24-bit colors
 
--- Other
-vim.o.spelllang = "ru_ru,en_us"
-vim.o.spell = false
-vim.o.inccommand = "split" -- Preview substitutions live, as you type!
-vim.o.virtualedit = "block" -- Allow going past the end of line in visual block mode
-vim.o.completeopt = "menuone,noselect" -- Customize completions
-vim.filetype.add {
-  pattern = {
-    [".*/.config/kitty/.*%.conf"] = "ini",
-  },
-}
+vim.o.conceallevel = 0 -- Visual-control chars (**, '') in md etc.
+
+vim.o.colorcolumn = "" -- Show column at "n" character
+
 vim.opt.shortmess:append "WcC" -- Reduce command line messages
+
 vim.o.splitkeep = "screen" -- Reduce scroll during window split
+
+-- Extra UI
+vim.o.pumblend = 10 -- Make builtin completion menus slightly transparent
+vim.o.pumheight = 10 -- Make popup menu smaller
+vim.o.winblend = 10 -- Make floating windows slightly transparent
+
 vim.o.listchars = "tab:> ,extends:…,precedes:…,nbsp:␣" -- Define which helper symbols to show
 vim.o.list = true -- Show some helper symbols
--- add binaries installed by mason.nvim to path
+
+if vim.fn.exists "syntax_on" ~= 1 then
+  vim.cmd "syntax enable" -- Enable syntax highlighting
+end
+
+-- Editing
+vim.o.ignorecase = true -- Case-insensitive search
+vim.o.infercase = true -- Infer letter cases for a richer built-in keyword completion
+vim.o.smartcase = true -- Case sensitive if uppercase in search
+vim.o.hlsearch = false -- Highlight search results
+vim.o.incsearch = true -- Show matches as you type
+
+vim.o.completeopt = "menuone,noselect" -- Customize completions
+vim.o.virtualedit = "block" -- Allow going past the end of line in visual block mode
+
+vim.o.inccommand = "split" -- Preview :substitute options
+
+-- Binaries
 local is_windows = vim.fn.has "win32" ~= 0
 local sep = is_windows and "\\" or "/"
 local delim = is_windows and ";" or ":"
