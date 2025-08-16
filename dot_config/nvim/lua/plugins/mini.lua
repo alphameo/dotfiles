f59973d69fcbc1a2d93c8f898fd7cb318ec5e586
alphameo <pavel.2014.play@gmail.com>
2025-08-17 00:40:17 +0300

    refactor(nvim): update mini

--- dot_config/nvim/lua/plugins/mini.lua
+++ dot_config/nvim/lua/plugins/mini.lua
@@ -9,6 +9,196 @@ vim.api.nvim_create_autocmd("TermOpen", {
     vim.b[args.buf].miniindentscope_disable = true
   end,
 })
+vim.api.nvim_create_autocmd("FileType", {
+  pattern = {
+    "help",
+    "startify",
+    "alpha",
+    "ministarter",
+    "dashboard",
+    "neo-tree",
+    "NvimTree",
+    "Trouble",
+    "lazy",
+    "packer",
+    "mason",
+    "toggleterm",
+    "neogitstatus",
+  },
+  callback = function()
+    vim.b.miniindentscope_disable = true
+  end,
+})
+
+local setup_sessions = function()
+  local sessions = require "mini.sessions"
+  sessions.setup()
+  vim.keymap.set("n", "<leader>wo", function()
+    sessions.select "read"
+  end, { desc = "Workspaces Open" })
+  vim.keymap.set("n", "<leader>wr", function()
+    sessions.select "write"
+  end, { desc = "Workspaces Replace" })
+  vim.keymap.set("n", "<leader>wd", function()
+    sessions.select "delete"
+  end, { desc = "Workspaces Delete" })
+  vim.keymap.set("n", "<leader>ws", function()
+    vim.ui.input({ prompt = "Workspace name: " }, function(input)
+      if input and input ~= "" then
+        sessions.write(input)
+      end
+    end)
+  end, { desc = "Workspaces Save" })
+end
+
+local setup_bracketed = function()
+  require("mini.bracketed").setup {
+    buffer = { suffix = "b", options = {} },
+    comment = { suffix = "c", options = {} },
+    conflict = { suffix = "x", options = {} },
+    diagnostic = { suffix = "" }, -- exists in diagnostics
+    file = { suffix = "f", options = {} },
+    indent = { suffix = "i", options = {} },
+    jump = { suffix = "" }, -- exists in mappings
+    location = { suffix = "l", options = {} },
+    oldfile = { suffix = "o", options = {} },
+    quickfix = { suffix = "q", options = {} },
+    treesitter = { suffix = "t", options = {} },
+    undo = { suffix = "u", options = {} },
+    window = { suffix = "w", options = {} },
+    yank = { suffix = "y", options = {} },
+  }
+end
+
+local setup_indentscope = function()
+  local indent = require "mini.indentscope"
+  indent.setup {
+    draw = {
+      delay = 0,
+      animation = indent.gen_animation.none(),
+    },
+    symbol = "▎",
+  }
+end
+
+local setup_sj = function()
+  local sj = require "mini.splitjoin"
+  sj.setup()
+  vim.keymap.set("n", "<leader>cj", sj.toggle, { desc = "Code SplitJoin" })
+end
+
+local setup_tabline = function()
+  local tabline = require "mini.tabline"
+  local function get_diagnostic_symbol(bufnr)
+    local levels = {
+      [vim.diagnostic.severity.ERROR] = " ",
+      [vim.diagnostic.severity.WARN] = " ",
+    }
+
+    local diagnostics = vim.diagnostic.get(bufnr)
+    local counts = {}
+
+    for _, d in ipairs(diagnostics) do
+      local severity = d.severity
+      counts[severity] = (counts[severity] or 0) + 1
+    end
+    local diagn = ""
+    for _, severity in ipairs {
+      vim.diagnostic.severity.ERROR,
+      vim.diagnostic.severity.WARN,
+    } do
+      if counts[severity] then
+        diagn = diagn .. levels[severity] .. counts[severity] .. " "
+      end
+    end
+
+    return diagn
+  end
+  -- local prefix = "▎"
+  local prefix = ""
+  tabline.setup {
+    format = function(buf_id, label)
+      local suffix = vim.bo[buf_id].modified and " " or ""
+      return prefix .. tabline.default_format(buf_id, label) .. get_diagnostic_symbol(buf_id) .. suffix
+    end,
+  }
+  vim.keymap.set("n", "<A-]>", ":bnext<CR>", { silent = true, desc = "Next Tab (buffer)" })
+  vim.keymap.set("n", "<C-Tab>", ":bnext<CR>", { silent = true, desc = "Next Tab (buffer)" })
+  vim.keymap.set("n", "<A-[>", ":bprev<CR>", { silent = true, desc = "Previous Tab (buffer)" })
+  vim.keymap.set("n", "<C-S-Tab>", ":bprev<CR>", { silent = true, desc = "Previous Tab (buffer)" })
+  vim.keymap.set("n", "<A-g>", ":Telescope buffers<CR>", { silent = true, desc = "Pick Tab (buffer) Next" })
+  vim.keymap.set("n", "<A-w>", ":Bdelete<CR>", { silent = true, desc = "Pick buffer to close" })
+  vim.keymap.set("n", "<A-\\>", "<C-^>", { silent = true, desc = "Switch to last buffer" })
+end
+
+local setup_statusline = function()
+  vim.api.nvim_set_hl(0, "MiniStatuslineFilename", {
+    fg = "#abb2bf",
+    bg = "#31353f",
+  })
+  local stl = require "mini.statusline"
+  stl.section_location = function()
+    return "%2l:%-2v"
+  end
+  stl.setup {
+    use_icons = true,
+    set_vim_settings = false, -- You already set vim.o.showmode = false
+    content = {
+      active = function()
+        local sep = "︙"
+        local mode, mode_hl = stl.section_mode { trunc_width = 120 }
+        local git = stl.section_git { trunc_width = 40 }
+        local diff = stl.section_diff { trunc_width = 75 }
+        local diagnostics = stl.section_diagnostics {
+          trunc_width = 75,
+          icon = "",
+          signs = { ERROR = " ", WARN = " ", INFO = " ", HINT = "󰌵 " },
+        }
+        -- local lsp = stl.section_lsp { trunc_width = 75 }
+        local filename = stl.section_filename { trunc_width = 140 }
+        local fileinfo = stl.section_fileinfo { trunc_width = 9999 }
+        local location = stl.section_location { trunc_width = 75 }
+        local search = stl.section_searchcount { trunc_width = 75 }
+
+        local noice_cmd = ""
+        if pcall(require, "noice") and require("noice").api.status.command.has() then
+          noice_cmd = require("noice").api.status.command.get()
+        end
+
+        local line = vim.fn.line "."
+        local total = vim.fn.line "$"
+        local percent = math.floor(line / total * 100)
+
+        local percentage = string.format("%d%%%%", percent)
+
+        return stl.combine_groups {
+          { hl = mode_hl, strings = { mode } },
+          { hl = "MiniStatuslineDevinfo", strings = { git, diff } },
+          "%<", -- general truncate point
+          { hl = "MiniStatuslineFilename", strings = { filename } },
+          "%=", -- end left alignment
+          { hl = "MiniStatuslineFilename", strings = {} },
+          { hl = "MiniStatuslineFilename", strings = { noice_cmd, sep, diagnostics } },
+          { hl = "MiniStatuslineFilename", strings = { fileinfo, search } },
+          { hl = "MiniStatuslineFileinfo", strings = { location } },
+          { hl = mode_hl, strings = { percentage } },
+        }
+      end,
+      inactive = function()
+        local filename = stl.section_filename { trunc_width = 140 }
+        local fileinfo = stl.section_fileinfo { trunc_width = 9999 }
+
+        return stl.combine_groups {
+          "%<", -- general truncate point
+          { hl = "MiniStatuslineFilename", strings = { filename } },
+          "%=", -- end left alignment
+          { hl = "MiniStatuslineFileinfo", strings = { fileinfo } },
+        }
+      end,
+    },
+  }
+end
+
 -- TODO: check mappings, when removing plugin
 return {
   "echasnovski/mini.nvim",
@@ -20,161 +210,14 @@ return {
   lazy = true,
   event = "VeryLazy",
   config = function()
-    local map = vim.keymap.set
-    require("mini.pairs").setup()
-
+    -- require("mini.pairs").setup()
     require("mini.surround").setup()
-
-    require("mini.move").setup()
-
-    require("mini.bracketed").setup {
-      buffer = { suffix = "b", options = {} },
-      comment = { suffix = "c", options = {} },
-      conflict = { suffix = "x", options = {} },
-      diagnostic = { suffix = "" }, -- exists in diagnostics
-      file = { suffix = "f", options = {} },
-      indent = { suffix = "i", options = {} },
-      jump = { suffix = "" }, -- exists in mappings
-      location = { suffix = "l", options = {} },
-      oldfile = { suffix = "o", options = {} },
-      quickfix = { suffix = "q", options = {} },
-      treesitter = { suffix = "t", options = {} },
-      undo = { suffix = "u", options = {} },
-      window = { suffix = "w", options = {} },
-      yank = { suffix = "y", options = {} },
-    }
-
-    local sessions = require "mini.sessions"
-    sessions.setup()
-    map("n", "<leader>wo", function()
-      sessions.select "read"
-    end, { desc = "Workspaces Open" })
-    map("n", "<leader>wr", function()
-      sessions.select "write"
-    end, { desc = "Workspaces Replace" })
-    map("n", "<leader>wd", function()
-      sessions.select "delete"
-    end, { desc = "Workspaces Delete" })
-    vim.keymap.set("n", "<leader>ws", function()
-      vim.ui.input({ prompt = "Workspace name: " }, function(input)
-        if input and input ~= "" then
-          sessions.write(input)
-        end
-      end)
-    end, { desc = "Workspaces Save" })
-
-    local indent = require "mini.indentscope"
-    indent.setup {
-      draw = {
-        delay = 0,
-        animation = indent.gen_animation.none(),
-      },
-      symbol = "▎",
-    }
-
-    local sj = require "mini.splitjoin"
-    sj.setup()
-    map("n", "<leader>cj", sj.toggle, { desc = "Code SplitJoin" })
-
-    vim.api.nvim_set_hl(0, "MiniTablineVisible", {
-      fg = "#777777",
-      bg = "#1e1e1e",
-    })
-
-    local tabline = require "mini.tabline"
-    local function get_diagnostic_symbol(bufnr)
-      local levels = {
-        [vim.diagnostic.severity.ERROR] = " ",
-        [vim.diagnostic.severity.WARN] = " ",
-      }
-
-      local diagnostics = vim.diagnostic.get(bufnr)
-      local counts = {}
-
-      for _, d in ipairs(diagnostics) do
-        local severity = d.severity
-        counts[severity] = (counts[severity] or 0) + 1
-      end
-      local diagn = ""
-      for _, severity in ipairs {
-        vim.diagnostic.severity.ERROR,
-        vim.diagnostic.severity.WARN,
-      } do
-        if counts[severity] then
-          diagn = diagn .. levels[severity] .. counts[severity] .. " "
-        end
-      end
-
-      return diagn
-    end
-    -- local prefix = "▎"
-    local prefix = ""
-    tabline.setup {
-      format = function(buf_id, label)
-        local suffix = vim.bo[buf_id].modified and " " or ""
-        return prefix .. tabline.default_format(buf_id, label) .. get_diagnostic_symbol(buf_id) .. suffix
-      end,
-    }
-    map("n", "<A-]>", ":bnext<CR>", { silent = true, desc = "Next Tab (buffer)" })
-    map("n", "<C-Tab>", ":bnext<CR>", { silent = true, desc = "Next Tab (buffer)" })
-    map("n", "<A-[>", ":bprev<CR>", { silent = true, desc = "Previous Tab (buffer)" })
-    map("n", "<C-S-Tab>", ":bprev<CR>", { silent = true, desc = "Previous Tab (buffer)" })
-    map("n", "<A-g>", ":Telescope buffers<CR>", { silent = true, desc = "Pick Tab (buffer) Next" })
-    map("n", "<A-w>", ":Bdelete<CR>", { silent = true, desc = "Pick buffer to close" })
-    map("n", "<A-\\>", "<C-^>", { silent = true, desc = "Switch to last buffer" })
-
-    local stl = require "mini.statusline"
-    stl.section_location = function()
-      return "%2l:%-2v"
-    end
-    stl.setup {
-      use_icons = true,
-      set_vim_settings = false, -- You already set vim.o.showmode = false
-      content = {
-        active = function()
-          local mode, mode_hl = stl.section_mode { trunc_width = 120 }
-          local git = stl.section_git { trunc_width = 40 }
-          local diff = stl.section_diff { trunc_width = 75 }
-          local diagnostics = stl.section_diagnostics {
-            trunc_width = 75,
-            icon = "",
-            signs = { ERROR = " ", WARN = " ", INFO = " ", HINT = "󰌵 " },
-          }
-          -- local lsp = stl.section_lsp { trunc_width = 75 }
-          local filename = stl.section_filename { trunc_width = 140 }
-          local fileinfo = stl.section_fileinfo { trunc_width = 9999 }
-          local location = stl.section_location { trunc_width = 75 }
-          local search = stl.section_searchcount { trunc_width = 75 }
-
-          local noice_cmd = ""
-          if pcall(require, "noice") and require("noice").api.status.command.has() then
-            noice_cmd = require("noice").api.status.command.get()
-          end
-
-          return stl.combine_groups {
-            { hl = mode_hl, strings = { mode } },
-            { hl = "MiniStatuslineDevinfo", strings = { git, diff } },
-            "%<", -- general truncate point
-            { hl = "MiniStatuslineFilename", strings = { filename } },
-            "%=", -- end left alignment
-            { hl = "MiniStatuslineFilename", strings = { noice_cmd } },
-            { hl = "MiniStatuslineDevinfo", strings = { diagnostics } },
-            { hl = "MiniStatuslineFileinfo", strings = { fileinfo } },
-            { hl = mode_hl, strings = { search, location } },
-          }
-        end,
-        inactive = function()
-          local filename = stl.section_filename { trunc_width = 140 }
-          local fileinfo = stl.section_fileinfo { trunc_width = 9999 }
-
-          return stl.combine_groups {
-            "%<", -- general truncate point
-            { hl = "MiniStatuslineFilename", strings = { filename } },
-            "%=", -- end left alignment
-            { hl = "MiniStatuslineFileinfo", strings = { fileinfo } },
-          }
-        end,
-      },
-    }
+    -- require("mini.move").setup()
+    setup_sessions()
+    -- setup_bracketed()
+    setup_indentscope()
+    -- setup_sj()
+    -- setup_tabline()
+    setup_statusline()
   end,
 }
