d2c2ae13a88f3a56037e9313c276d67e8e6f0e23
alphameo <pavel.2014.play@gmail.com>
2025-08-17 00:39:30 +0300

    refactor(nvim): change listchars in options

--- dot_config/nvim/lua/options.lua
+++ dot_config/nvim/lua/options.lua
@@ -66,7 +66,7 @@ vim.o.pumblend = 10 -- Make builtin completion menus slightly transparent
 vim.o.pumheight = 10 -- Make popup menu smaller
 vim.o.winblend = 10 -- Make floating windows slightly transparent
 
-vim.o.listchars = "tab:> ,extends:…,precedes:…,nbsp:␣" -- Define which helper symbols to show
+vim.o.listchars = "tab:> ,extends:…,precedes:…,nbsp:" -- Define which helper symbols to show
 vim.o.list = false -- Show some helper symbols
 
 if vim.fn.exists "syntax_on" ~= 1 then
