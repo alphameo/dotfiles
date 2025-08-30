f2f81a2d22f51fb7ee9ac0d026930900515bdf07
alphameo <pavel.2014.play@gmail.com>
2025-08-30 12:31:59 +0300

    refactor(nvim): avoid mason-dap, split mason and its tool-installer

--- dot_config/nvim/lua/plugins/core/mason.lua
+++ dot_config/nvim/lua/plugins/core/mason.lua
@@ -1,116 +1,115 @@
 vim.keymap.set({ "n" }, "<leader>M", ":Mason<CR>", { silent = true, desc = "Mason" })
 
 return {
-  "williamboman/mason.nvim",
-  dependencies = {
-    "williamboman/mason-lspconfig.nvim",
-    "WhoIsSethDaniel/mason-tool-installer.nvim",
-    "jay-babu/mason-nvim-dap.nvim",
-  },
-  build = ":MasonUpdate",
-  lazy = true,
-  cmd = { "Mason", "MasonInstall", "MasonUpdate" },
-  config = function()
-    local mason = require "mason"
-    local mason_lspconfig = require "mason-lspconfig"
-    local mason_dap = require "mason-nvim-dap"
-    local mason_tool_installer = require "mason-tool-installer"
-
-    mason.setup {
-      ui = {
-        icons = {
-          package_pending = " ",
-          package_installed = " ",
-          package_uninstalled = " ",
+  {
+    "williamboman/mason.nvim",
+    build = ":MasonUpdate",
+    lazy = true,
+    cmd = { "Mason", "MasonInstall", "MasonUpdate" },
+    config = function()
+      require("mason").setup {
+        ui = {
+          icons = {
+            package_pending = " ",
+            package_installed = " ",
+            package_uninstalled = " ",
+          },
         },
-      },
-
-      max_concurrent_installers = 10,
-    }
-
-    mason_lspconfig.setup {
-      ensure_installed = {
-        "lua_ls", -- lua
-        "jdtls", -- java
-        "clangd", -- c, cpp
-        "gopls", -- go
-        "neocmake", -- cmake
-        "marksman", -- markdown
-        "texlab", -- LaTeX
-        "kotlin_language_server", -- kotlin
-        "html", -- html
-        "cssls", -- css
-        "cssmodules_ls", -- css
-        "css_variables", -- css
-        "tailwindcss", -- tailwind
-        "ts_ls", -- javascript, typescript
-        -- "denols", -- javascript, typescript
-        -- "vtsls", -- typescript
-        -- "pyright", -- python
-        "basedpyright", -- python static checker
-        "lemminx", -- xml
-        "taplo", -- toml
-        "jsonls", -- json
-        "yamlls", -- yaml
-        "sqlls", -- sql
-        "bashls", -- bash
-        "intelephense", -- php
-        "phpactor", -- php
-        "rust_analyzer", -- rust
-      },
-    }
-
-    mason_dap.setup {
-      ensure_installed = {
-        "javadbg", -- java-debug-adapter
-        "javatest", -- java-test
-        "cppdbg", -- c, cpp
-        "codelldb", -- c, cpp, rust
-        "python", -- python
-        "js", -- javascript, typescript
-        "firefox", -- javascript, typescript
-        "kotlin", -- kotlin
-        "delve", -- go
-        "php", -- php
-      },
-    }
-
-    mason_tool_installer.setup {
-      ensure_installed = {
-        "prettierd", -- ts, js, ... fmt
-        "eslint_d", -- ts, js, ... lint
-        "stylua", -- lua fmt
-        "luacheck", -- lua lint
-        "ktlint", -- kotlin fmt/lint
-        "detekt", -- kotlin lint
-        "clang-format", -- c, cpp fmt
-        "cpplint", -- c, cpp lint
-        "gersemi", -- cmake fmt
-        "cmakelint", -- cmake lint
-        "ruff", -- python fmt
-        "black", -- python fmt
-        "mypy", -- python lint
-        "markdownlint-cli2", -- markdown fmt/lint
-        "markdown-toc", -- markdown fmt
-        "gofumpt", -- go fmt
-        "goimports", -- go fmt
-        "golangci-lint", -- go lint
-        "php-cs-fixer", -- php fmt
-        "phpcs", -- php lint
-        "rustfmt", -- rust fmt
-        -- "bacon-ls", -- rust
-        -- "bacon", -- rust lint
-        "shfmt", -- bash fmt
-        -- "stylelint", -- css lint
-        "htmlhint", -- html lint
-        -- "jsonlint", -- json lint
-        "sqlfluff", -- sql lint
-        "sqlfmt", -- sql fmt
-        -- "vale", -- tex lint
-        "bibtex-tidy", -- tex fmt
-        -- "xmlformatter", -- xml fmt
-        -- "yamllint", -- yaml lint
-      },
-    }
-  end,
+        max_concurrent_installers = 10,
+      }
+    end,
+  },
+  {
+    "WhoIsSethDaniel/mason-tool-installer.nvim",
+    dependencies = {
+      "williamboman/mason-lspconfig.nvim",
+    },
+    lazy = true,
+    cmd = {
+      "MasonToolsInstall",
+      "MasonToolsInstallSync",
+      "MasonToolsUpdate",
+      "MasonToolsUpdateUpdate",
+      "MasonToolsClean",
+    },
+    config = function()
+      require("mason-tool-installer").setup {
+        ensure_installed = {
+          -- LSPs
+          "lua_ls", -- lua
+          "jdtls", -- java
+          "clangd", -- c, cpp
+          "gopls", -- go
+          "neocmake", -- cmake
+          "marksman", -- markdown
+          "texlab", -- LaTeX
+          "kotlin_language_server", -- kotlin
+          "html", -- html
+          "cssls", -- css
+          "cssmodules_ls", -- css
+          "css_variables", -- css
+          "tailwindcss", -- tailwind
+          "ts_ls", -- javascript, typescript
+          -- "denols", -- javascript, typescript
+          -- "vtsls", -- typescript
+          -- "pyright", -- python
+          "basedpyright", -- python static checker
+          "lemminx", -- xml
+          "taplo", -- toml
+          "jsonls", -- json
+          "yamlls", -- yaml
+          "sqlls", -- sql
+          "bashls", -- bash
+          "intelephense", -- php
+          "phpactor", -- php
+          "rust_analyzer", -- rust
+          -- Debuggers
+          "java-debug-adapter",
+          "java-test",
+          "cpptools", -- c, cpp
+          "codelldb", -- c, cpp, rust
+          "debugpy", -- python
+          "js-debug-adapter", -- javascript, typescript
+          "firefox-debug-adapter", -- javascript, typescript
+          "kotlin-debug-adapter",
+          "delve", -- go
+          "php-debug-adapter",
+          -- Formatters & Linters
+          "prettierd", -- ts, js, ... fmt
+          "eslint_d", -- ts, js, ... lint
+          "stylua", -- lua fmt
+          "luacheck", -- lua lint
+          "ktlint", -- kotlin fmt/lint
+          "detekt", -- kotlin lint
+          "clang-format", -- c, cpp fmt
+          "cpplint", -- c, cpp lint
+          "gersemi", -- cmake fmt
+          "cmakelint", -- cmake lint
+          "ruff", -- python fmt
+          "black", -- python fmt
+          "mypy", -- python lint
+          "markdownlint-cli2", -- markdown fmt/lint
+          "markdown-toc", -- markdown fmt
+          "gofumpt", -- go fmt
+          "goimports", -- go fmt
+          "golangci-lint", -- go lint
+          "php-cs-fixer", -- php fmt
+          "phpcs", -- php lint
+          "rustfmt", -- rust fmt
+          -- "bacon-ls", -- rust
+          -- "bacon", -- rust lint
+          "shfmt", -- bash fmt
+          -- "stylelint", -- css lint
+          "htmlhint", -- html lint
+          -- "jsonlint", -- json lint
+          "sqlfluff", -- sql lint
+          "sqlfmt", -- sql fmt
+          -- "vale", -- tex lint
+          "bibtex-tidy", -- tex fmt
+          -- "xmlformatter", -- xml fmt
+          -- "yamllint", -- yaml lint
+        },
+      }
+    end,
+  },
 }
