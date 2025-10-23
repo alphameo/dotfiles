20fbe1c4a70c9bfd408898019d60b87785a1d3fa
alphameo <pavel.2014.play@gmail.com>
2025-10-24 01:17:53 +0300

    feat(nvim): add plugin nvim-lsp-file-operations

--- dot_config/nvim/lua/plugins/core/lspconfig.lua
+++ dot_config/nvim/lua/plugins/core/lspconfig.lua
@@ -1,12 +1,31 @@
 return {
-  "neovim/nvim-lspconfig",
-  lazy = true,
-  event = { "FileType" },
-  opts = {
-    inlay_hints = { enabled = true },
-    codelens = { enabled = true },
+  {
+    "neovim/nvim-lspconfig",
+    lazy = true,
+    event = { "FileType" },
+    opts = {
+      inlay_hints = { enabled = true },
+      codelens = { enabled = true },
+    },
+    config = function()
+      require("lsp").setup()
+    end,
   },
-  config = function()
-    require("lsp").setup()
-  end,
+  {
+    {
+      "antosha417/nvim-lsp-file-operations",
+      lazy = true,
+      event = { "FileType" },
+      dependencies = {
+        "nvim-lua/plenary.nvim",
+        -- Uncomment whichever supported plugin(s) you use
+        -- "nvim-tree/nvim-tree.lua",
+        "nvim-neo-tree/neo-tree.nvim",
+        -- "simonmclean/triptych.nvim"
+      },
+      config = function()
+        require("lsp-file-operations").setup()
+      end,
+    },
+  },
 }
