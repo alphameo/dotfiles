0ed0e5b485af6dbf1a7c807f08b98213aebbfad1
alphameo <pavel.2014.play@gmail.com>
2025-12-26 15:46:11 +0300

    refactor(nvim): change listchar

--- dot_config/nvim/lua/options.lua
+++ dot_config/nvim/lua/options.lua
@@ -72,8 +72,19 @@ vim.o.pumblend = 10 -- Make builtin completion menus slightly transparent
 vim.o.pumheight = 10 -- Make popup menu smaller
 -- vim.o.winblend = 10 -- Make floating windows slightly transparent (causes bugs with block cursor in overlays)
 
-vim.o.listchars = "tab:> ,extends:…,precedes:…,nbsp:" -- Define which helper symbols to show
-vim.o.list = false -- Show some helper symbols
+vim.opt.showbreak = "↳"
+local space = "·"
+vim.opt.listchars = {
+  multispace = space,
+  lead = space,
+  trail = space,
+  nbsp = space,
+  eol = "⏎",
+  tab = "› ",
+  extends = "…",
+  precedes = "…",
+}
+vim.opt.list = false -- Show non-printing characters (glyphs)
 
 if vim.fn.exists "syntax_on" ~= 1 then
   vim.cmd "syntax enable" -- Enable syntax highlighting
