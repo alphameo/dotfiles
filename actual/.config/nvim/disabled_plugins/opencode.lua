ec0b6341e11fc559d361a2f35e89bfd37acee012
alphameo <pavel.2014.play@gmail.com>
2026-01-13 14:33:18 +0300

    refactor(nvim): change deprecated opencode mapping

--- actual/.config/nvim/disabled_plugins/opencode.lua
+++ actual/.config/nvim/disabled_plugins/opencode.lua
@@ -2,6 +2,9 @@ return {
   "NickvanDyke/opencode.nvim",
   lazy = true,
   event = "VeryLazy",
+  dependencies = {
+    { "folke/snacks.nvim", opts = { input = {}, picker = {}, terminal = {} } },
+  },
   config = function()
     vim.g.opencode_opts = {}
 
@@ -13,15 +16,15 @@ return {
     map({ "n", "t" }, "<M-`>", function()
       opencode.toggle()
     end, { desc = "AI Agent" })
-    map({ "n", "x" }, "gaa", function()
+    map({ "n", "x" }, "<leader>aa", function()
       opencode.select()
-    end, { desc = "Opencode actions" })
+    end, { desc = "Opencode Actions" })
 
-    map({ "n", "x" }, "gar", function()
+    map({ "n", "x" }, "<leader>ar", function()
       return opencode.operator "@this "
-    end, { expr = true, desc = "Opencode add range" })
-    map("n", "gal", function()
+    end, { expr = true, desc = "Opencode add Range" })
+    map("n", "<leader>al", function()
       return opencode.operator "@this " .. "_"
-    end, { expr = true, desc = "Opencode add line" })
+    end, { expr = true, desc = "Opencode add Line" })
   end,
 }
