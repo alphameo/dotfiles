7f2fdb683ae5ed9b769c793c5466d2593f4372ca
alphameo <pavel.2014.play@gmail.com>
2026-02-13 12:43:10 +0300

    refactor(nvim): update image plugin
    refactor(nvim): image toggling mapping desc style

--- actual/.config/nvim/lua/plugins/image.lua
+++ actual/.config/nvim/lua/plugins/image.lua
@@ -3,14 +3,31 @@ return {
   lazy = true,
   ft = { "markdown" },
   config = function()
-    require("image").setup {
+    local img = require "image"
+    img.setup {
       backend = "kitty",
       max_width = 128,
       max_height = 16,
       max_height_window_percentage = math.huge,
       max_width_window_percentage = math.huge,
       window_overlap_clear_enabled = true,
-      window_overlap_clear_ft_ignore = { "cmp_menu", "cmp_docs", "" },
+      integrations = {
+        markdown = {
+          only_render_image_at_cursor = false, -- defaults to false
+          only_render_image_at_cursor_mode = "popup", -- "popup" or "inline", defaults to "popup"
+        },
+      },
     }
+
+    local toggle_img = function()
+      if require("image").is_enabled() then
+        require("image").disable()
+        vim.notify("Images disabled", vim.log.levels.INFO)
+      else
+        require("image").enable()
+        vim.notify("Images enabled", vim.log.levels.INFO)
+      end
+    end
+    vim.keymap.set("n", "\\i", toggle_img, { desc = "Toggle Image Previews" })
   end,
 }
