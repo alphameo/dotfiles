cffc304a6e032428a664ee39fb42aca5b39124a1
alphameo <pavel.2014.play@gmail.com>
2026-02-13 14:59:23 +0300

    refactor(rofi): upgrade update manager functionality

--- actual/.config/rofi/scripts/update_manager.sh
+++ actual/.config/rofi/scripts/update_manager.sh
@@ -5,29 +5,36 @@ ALL=$ICON"All"
 HYPR=$ICON"hypr"
 YAZI=$ICON"yazi"
 PACKAGES=$ICON"packages"
+ZSH_ZINIT=$ICON"zsh zinit"
 
 function run_in_term {
-    coproc ($TERMINAL --hold "$@")
+    coproc ($TERMINAL --hold zsh -c "$@")
     exit 0
 }
 
+yazi_upd="ya pkg upgrade"
+hypr_upd="hyprpm update"
+packages_upd="yay"
+zinit_upd="source ~/.config/zsh/.zshrc && zinit self-update && zinit update"
+
 function update_yazi {
-    run_in_term "ya" "pkg" "upgrade"
+    run_in_term "echo 'Updating yazi 󰇥 '; $yazi_upd"
 }
 
 function update_hypr {
-    run_in_term "hyprpm" "update"
+    run_in_term "echo 'Updating hypr  '; $hypr_upd"
 }
 
 function update_packages {
-    run_in_term "yay"
+    run_in_term "echo 'Updating packages '; $packages_upd"
 }
 
+function update_zinit {
+    run_in_term "echo 'Updating zinit  '; $zinit_upd"
+}
 
 function update_all {
-    update_packages
-    update_yazi
-    update_hypr
+    run_in_term "echo 'Updating all package managers  ';$packages_upd; $hypr_upd; $zinit_upd; $yazi_upd"
 }
 
 function run {
@@ -38,6 +45,8 @@ function run {
         update_yazi
     elif [[ $INP == $PACKAGES ]]; then
         update_packages
+    elif [[ $INP == $ZSH_ZINIT ]]; then
+        update_zinit
     elif [[ $INP == $ALL ]]; then
         update_all
     else
@@ -45,6 +54,7 @@ function run {
         echo $PACKAGES
         echo $HYPR
         echo $YAZI
+        echo $ZSH_ZINIT
         exit 0
     fi
 }
