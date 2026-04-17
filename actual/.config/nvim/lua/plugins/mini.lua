5fca65acb9b9620a1893f40a6654f995a6ba0336
alphameo <pavel.2014.play@gmail.com>
2026-04-17 11:26:11 +0300

    refactor(nvim): disable mini surround mappings

--- actual/.config/nvim/lua/plugins/mini.lua
+++ actual/.config/nvim/lua/plugins/mini.lua
@@ -39,11 +39,11 @@ local setup_surround = function()
       delete = "ds", -- Delete surrounding
       find = "", -- Find surrounding (to the right)
       find_left = "", -- Find surrounding (to the left)
-      -- highlight = "s", -- Highlight surrounding
+      highlight = "", -- Highlight surrounding
       replace = "cs", -- Replace surrounding
 
-      -- suffix_last = "l", -- Suffix to search with "prev" method
-      -- suffix_next = "n", -- Suffix to search with "next" method
+      suffix_last = "", -- Suffix to search with "prev" method
+      suffix_next = "", -- Suffix to search with "next" method
     },
     n_lines = 20,
     respect_selection_type = false,
