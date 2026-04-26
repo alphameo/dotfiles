06761ee4702d8da462f88bef7641e2de0c15f9be
alphameo <pavel.2014.play@gmail.com>
2026-04-26 10:54:19 +0300

    refactor(nvim): update git-signs mappings

--- actual/.config/nvim/lua/plugins/git-signs.lua
+++ actual/.config/nvim/lua/plugins/git-signs.lua
@@ -24,14 +24,23 @@ return {
       word_diff = false,
     }
 
+    local gitsigns = require "gitsigns"
+
     map("n", "<leader>gb", ":Gitsigns blame<CR>", { silent = true, desc = "Git Blame" })
-    map("n", "gh", ":Gitsigns preview_hunk_inline<CR>", { silent = true, desc = "Git Hunk Preview" })
+    map("n", "gh", gitsigns.preview_hunk_inline, { silent = true, desc = "Git Hunk Preview Inline" })
+    map("n", "gH", gitsigns.preview_hunk, { silent = true, desc = "Git Hunk Preview Window" })
     map("n", "]h", ":Gitsigns nav_hunk next<CR>", { silent = true, desc = "Go to Next git Hunk" })
     map("n", "[h", ":Gitsigns nav_hunk prev<CR>", { silent = true, desc = "Go to Previous git Hunk" })
-    map("n", "\\h", ":Gitsigns stage_hunk<CR>", { silent = true, desc = "Toggle Git Stage Hunk" })
-    map("n", "<leader>gr", ":Gitsigns reset_hunk<CR>", { silent = true, desc = "Git Reset Hunk" })
+    map("n", "<leader>gs", gitsigns.stage_hunk, { silent = true, desc = "Toggle Git Stage Hunk" })
+    map("n", "<leader>gr", gitsigns.reset_hunk, { silent = true, desc = "Git Reset Hunk" })
+    map("n", "<leader>gS", gitsigns.stage_buffer, { silent = true, desc = "Toggle Git Stage Buffer" })
+    map("n", "<leader>gR", gitsigns.reset_buffer, { silent = true, desc = "Git Reset Buffer" })
+    map("n", "<leader>gH", gitsigns.setqflist, { desc = "Git Hunk List" })
+    map("n", "<leader>gD", ":Gitsigns show ", { desc = "Git show <REVISION>" })
+    map("n", "<leader>gd", ":Gitsigns diffthis  ", { desc = "Git diff with <REVISION>" })
+    map("n", "<leader>gb", ":Gitsigns change_base ", { desc = "Git Change Base <REVISION>" })
 
-    cmd("GitBlame", "Gitsigns toggle_current_line_blame", { desc = "Toggle Git Blame Line" })
-    cmd("GitWordDiff", "Gitsigns toggle_word_diff", { desc = "Toggle Git Word Diff" })
+    cmd("GitBlame", gitsigns.toggle_current_line_blame, { desc = "Toggle Git Blame Line" })
+    cmd("GitWordDiff", gitsigns.toggle_word_diff, { desc = "Toggle Git Word Diff" })
   end,
 }
