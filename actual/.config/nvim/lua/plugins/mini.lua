7536c9f23d1f8568e0e62dd652cf08379e51e532
alphameo <pavel.2014.play@gmail.com>
2026-05-15 21:11:52 +0300

    refactor(nvim): replace mini surround with nvim-surround

--- actual/.config/nvim/lua/plugins/mini.lua
+++ actual/.config/nvim/lua/plugins/mini.lua
@@ -279,7 +279,7 @@ return {
   event = "VeryLazy",
   config = function()
     -- require("mini.pairs").setup()
-    setup_surround()
+    -- setup_surround()
     -- require("mini.move").setup()
     -- setup_sessions()
     -- setup_bracketed()
