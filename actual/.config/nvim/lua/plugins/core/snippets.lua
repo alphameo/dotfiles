11b1f58dc4d8f9b9ad6f7c1b70741025ae5a9cea
alphameo <pavel.2014.play@gmail.com>
2026-05-28 12:47:20 +0300

    feat(nvim): add 2 sources of custom snippets

--- actual/.config/nvim/lua/plugins/core/snippets.lua
+++ actual/.config/nvim/lua/plugins/core/snippets.lua
@@ -18,6 +18,10 @@ return {
     lazy = true,
     config = function()
       require("luasnip.loaders.from_vscode").lazy_load()
+      require("luasnip.loaders.from_vscode").lazy_load { paths = { "./snippets" } }
+      require("luasnip.loaders.from_lua").lazy_load {
+        paths = vim.fn.stdpath "config" .. "/lua/snippets",
+      }
     end,
   },
 }
