cbe67f9b715ad032dd2f6c5ab0c9226646541ffc
alphameo <pavel.2014.play@gmail.com>
2026-06-05 19:31:56 +0300

    refactor(nvim): mv typst lsp mappings into ftplugin

--- actual/.config/nvim/ftplugin/typst.lua
+++ actual/.config/nvim/ftplugin/typst.lua
@@ -8,3 +8,27 @@ opt.conceallevel = 2
 local map = vim.keymap.set
 map("n", "j", "gj", { buffer = true })
 map("n", "k", "gk", { buffer = true })
+map("n", "<leader>les", ":LspTinymistExportSvg<CR>", { buffer = true, silent = true, desc = "Typst Export SVG" })
+map("n", "<leader>lep", ":LspTinymistExportPng<CR>", { buffer = true, silent = true, desc = "Typst Export PNG" })
+map("n", "<leader>leP", ":LspTinymistExportPdf<CR>", { buffer = true, silent = true, desc = "Typst Export PDF" })
+map("n", "<leader>lem", ":LspTinymistExportPdf<CR>", { buffer = true, silent = true, desc = "Typst Export Markdown" })
+map("n", "<leader>li", ":LspTinymistGetServerInfo<CR>", { buffer = true, silent = true, desc = "Typst server Info" })
+map(
+  "n",
+  "<leader>lt",
+  ":LspTinymistGetDocumentTrace<CR>",
+  { buffer = true, silent = true, desc = "Typst document Trace" }
+)
+map(
+  "n",
+  "<leader>ll",
+  ":LspTinymistGetWorkspaceLabels<CR>",
+  { buffer = true, silent = true, desc = "Typst workspace Labels" }
+)
+map(
+  "n",
+  "<leader>lm",
+  ":LspTinymistGetDocumentMetrics<CR>",
+  { buffer = true, silent = true, desc = "Typst document Metrics" }
+)
+map("n", "<leader>lM", ":LspTinymistPinMain<CR>", { buffer = true, silent = true, desc = "Typst pin Main" })
--- actual/.config/nvim/lua/plugins/lang-typst.lua
+++ actual/.config/nvim/lua/plugins/lang-typst.lua
@@ -21,55 +21,6 @@ return {
           ":TypstPreviewToggle<CR>",
           { buffer = true, silent = true, desc = "Typst Preview Toggle" }
         )
-        map(
-          "n",
-          "<leader>les",
-          ":LspTinymistExportSvg<CR>",
-          { buffer = true, silent = true, desc = "Typst Export SVG" }
-        )
-        map(
-          "n",
-          "<leader>lep",
-          ":LspTinymistExportPng<CR>",
-          { buffer = true, silent = true, desc = "Typst Export PNG" }
-        )
-        map(
-          "n",
-          "<leader>leP",
-          ":LspTinymistExportPdf<CR>",
-          { buffer = true, silent = true, desc = "Typst Export PDF" }
-        )
-        map(
-          "n",
-          "<leader>lem",
-          ":LspTinymistExportPdf<CR>",
-          { buffer = true, silent = true, desc = "Typst Export Markdown" }
-        )
-        map(
-          "n",
-          "<leader>li",
-          ":LspTinymistGetServerInfo<CR>",
-          { buffer = true, silent = true, desc = "Typst server Info" }
-        )
-        map(
-          "n",
-          "<leader>lt",
-          ":LspTinymistGetDocumentTrace<CR>",
-          { buffer = true, silent = true, desc = "Typst document Trace" }
-        )
-        map(
-          "n",
-          "<leader>ll",
-          ":LspTinymistGetWorkspaceLabels<CR>",
-          { buffer = true, silent = true, desc = "Typst workspace Labels" }
-        )
-        map(
-          "n",
-          "<leader>lm",
-          ":LspTinymistGetDocumentMetrics<CR>",
-          { buffer = true, silent = true, desc = "Typst document Metrics" }
-        )
-        map("n", "<leader>lM", ":LspTinymistPinMain<CR>", { buffer = true, silent = true, desc = "Typst pin Main" })
       end,
     })
   end,
