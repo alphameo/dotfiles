ae98ee03f3f00289da2744e5ad110ed999497550
alphameo <pavel.2014.play@gmail.com>
2026-06-23 12:49:25 +0300

    nvim: add onedarkpro theme to disabled

---
+++ actual/.config/nvim/disabled_plugins/onedark-pro.lua
@@ -0,0 +1,7 @@
+return {
+  "olimorris/onedarkpro.nvim",
+  priority = 1000, -- make sure to load this before all the other start plugins
+  config = function()
+    vim.cmd "colorscheme onedark"
+  end,
+}
