809e2ff5fd0faac65aea271c45d1813da60e80ac
alphameo <pavel.2014.play@gmail.com>
2026-08-04 14:36:22 +0300

    sh: reformat aliases

--- actual/.config/sh/extended_aliases.sh
+++ actual/.config/sh/extended_aliases.sh
@@ -73,11 +73,9 @@ alias sha1='openssl sha1'
 alias clickpaste='sleep 3; xdotool type "$(xclip -o -selection clipboard)"'
 
 # KITTY - alias to be able to use kitty features when connecting to remote servers(e.g use tmux on remote server)
-
 alias kssh="kitty +kitten ssh"
 
-# alias to cleanup unused docker containers, images, networks, and volumes
-
+# Alias to cleanup unused docker containers, images, networks, and volumes
 alias docker-clean=' \
   docker container prune -f ; \
   docker image prune -f ; \
