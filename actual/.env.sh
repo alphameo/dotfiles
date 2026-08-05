ee2a27c696c23afd4993ba4eedefa6de04a18bd0
alphameo <pavel.2014.play@gmail.com>
2026-08-06 00:31:56 +0300

    env: mv quotes

--- actual/.env.sh
+++ actual/.env.sh
@@ -38,7 +38,7 @@ export TEXMFCONFIG="$XDG_CONFIG_HOME/texlive/texmf-config"
 
 export XCURSOR_PATH="${XCURSOR_PATH}:${XDG_DATA_HOME}/icons"
 
-mkdir -p "$XDG_DATA_HOME"/wineprefixes
+mkdir -p "$XDG_DATA_HOME/wineprefixes"
 export WINEPREFIX="$XDG_DATA_HOME/wineprefixes/default"
 
 # Native file dialog for electron apps
@@ -75,10 +75,17 @@ export CLICOLOR=1
 export LS_COLORS='no=00:fi=00:di=00;34:ln=31;36:pi=40;33:so=01;35:do=01;35:bd=40;33;01:cd=40;33;01:or=40;31;01:ex=01;32:*.tar=01;31:*.tgz=01;31:*.arj=01;31:*.taz=01;31:*.lzh=01;31:*.zip=01;31:*.z=01;31:*.Z=01;31:*.gz=01;31:*.bz2=01;31:*.deb=01;31:*.rpm=01;31:*.jar=01;31:*.jpg=01;35:*.jpeg=01;35:*.gif=01;35:*.bmp=01;35:*.pbm=01;35:*.pgm=01;35:*.ppm=01;35:*.tga=01;35:*.xbm=01;35:*.xpm=01;35:*.tif=01;35:*.tiff=01;35:*.png=01;35:*.mov=01;35:*.mpg=01;35:*.mpeg=01;35:*.avi=01;35:*.fli=01;35:*.gl=01;35:*.dl=01;35:*.xcf=01;35:*.xwd=01;35:*.ogg=01;35:*.mp3=01;35:*.wav=01;35:*.xml=00;31:'
 
 # For less theming
+# begin blinking
 export LESS_TERMCAP_mb=$'\E[01;31m'
+# begin bold
 export LESS_TERMCAP_md=$'\E[01;31m'
+# reset
 export LESS_TERMCAP_me=$'\E[0m'
+# end standout
 export LESS_TERMCAP_se=$'\E[0m'
+# standout
 export LESS_TERMCAP_so=$'\E[01;44;33m'
+# end underline
 export LESS_TERMCAP_ue=$'\E[0m'
+# begin underline
 export LESS_TERMCAP_us=$'\E[01;32m'
