3f234e843df47d560b14cc6175886b9506bf8dc1
alphameo <pavel.2014.play@gmail.com>
2026-08-10 00:26:34 +0300

    yazi: restyle

--- actual/.config/yazi/init.lua
+++ actual/.config/yazi/init.lua
@@ -1,28 +1,28 @@
 -- INFO: update: $ ya pkg upgrade
 
-require("full-border"):setup({
-	type = ui.Border.ROUNDED,
-})
+require("full-border"):setup {
+  type = ui.Border.ROUNDED,
+}
 
 require("git"):setup()
 require("recycle-bin"):setup()
 
 -- Show symlink in status bar
 Status:children_add(function(self)
-	local h = self._current.hovered
-	if h and h.link_to then
-		return " -> " .. tostring(h.link_to)
-	else
-		return ""
-	end
+  local h = self._current.hovered
+  if h and h.link_to then
+    return " -> " .. tostring(h.link_to)
+  else
+    return ""
+  end
 end, 3300, Status.LEFT)
 
-require("starship"):setup({
-	-- Hide flags (such as filter, find and search). This is recommended for starship themes which
-	-- are intended to go across the entire width of the terminal.
-	hide_flags = false, -- Default: false
-	-- Whether to place flags after the starship prompt. False means the flags will be placed before the prompt.
-	flags_after_prompt = true, -- Default: true
-	-- Custom starship configuration file to use
-	config_file = os.getenv "STARSHIP_CONFIG" or "~/.config/starship.toml", -- Default: nil
-})
+require("starship"):setup {
+  -- Hide flags (such as filter, find and search). This is recommended for starship themes which
+  -- are intended to go across the entire width of the terminal.
+  hide_flags = false, -- Default: false
+  -- Whether to place flags after the starship prompt. False means the flags will be placed before the prompt.
+  flags_after_prompt = true, -- Default: true
+  -- Custom starship configuration file to use
+  config_file = os.getenv "STARSHIP_CONFIG" or "~/.config/starship.toml", -- Default: nil
+}
