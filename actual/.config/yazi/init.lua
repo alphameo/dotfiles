149016eec19cb0b710f45ac58bbc3780627e0956
alphameo <pavel.2014.play@gmail.com>
2026-08-30 12:30:15 +0300

    yazi: add info into bottom

--- actual/.config/yazi/init.lua
+++ actual/.config/yazi/init.lua
@@ -11,6 +11,16 @@ require("git"):setup {
 
 require("recycle-bin"):setup()
 
+require("starship"):setup {
+  -- Hide flags (such as filter, find and search). This is recommended for starship themes which
+  -- are intended to go across the entire width of the terminal.
+  hide_flags = false, -- Default: false
+  -- Whether to place flags after the starship prompt. False means the flags will be placed before the prompt.
+  flags_after_prompt = true, -- Default: true
+  -- Custom starship configuration file to use
+  config_file = os.getenv "STARSHIP_CONFIG" or "~/.config/starship.toml", -- Default: nil
+}
+
 -- Show symlink in status bar
 Status:children_add(function(self)
   local h = self._current.hovered
@@ -21,12 +31,17 @@ Status:children_add(function(self)
   end
 end, 3300, Status.LEFT)
 
-require("starship"):setup {
-  -- Hide flags (such as filter, find and search). This is recommended for starship themes which
-  -- are intended to go across the entire width of the terminal.
-  hide_flags = false, -- Default: false
-  -- Whether to place flags after the starship prompt. False means the flags will be placed before the prompt.
-  flags_after_prompt = true, -- Default: true
-  -- Custom starship configuration file to use
-  config_file = os.getenv "STARSHIP_CONFIG" or "~/.config/starship.toml", -- Default: nil
-}
+-- Show user/group of files in status bar
+Status:children_add(function()
+  local h = cx.active.current.hovered
+  if not h or ya.target_family() ~= "unix" then
+    return ""
+  end
+
+  return ui.Line {
+    ui.Span(ya.user_name(h.cha.uid) or tostring(h.cha.uid)):fg "magenta",
+    ":",
+    ui.Span(ya.group_name(h.cha.gid) or tostring(h.cha.gid)):fg "magenta",
+    " ",
+  }
+end, 500, Status.RIGHT)
