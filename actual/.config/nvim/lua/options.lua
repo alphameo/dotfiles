a47e367d04a3759f9728e47340698bda1464c3b8
alphameo <pavel.2014.play@gmail.com>
2026-09-07 23:09:44 +0300

    nvim: disable default cmp

--- actual/.config/nvim/lua/options.lua
+++ actual/.config/nvim/lua/options.lua
@@ -58,10 +58,10 @@ vim.o.autoindent = true
 vim.o.breakindent = true
 
 -- Completion
-vim.o.autocomplete = true
+vim.o.autocomplete = false
 vim.o.completeopt = "menuone,popup,noselect,noinsert,fuzzy"
 vim.o.complete = "o,w,b,u,t"
-vim.o.wildmenu = true
+vim.o.wildmenu = false
 vim.o.wildmode = "noselect:full"
 vim.o.wildoptions = "pum,fuzzy,tagfile"
 
