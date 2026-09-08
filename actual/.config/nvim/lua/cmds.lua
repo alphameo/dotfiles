f90ac19f0cdf2a66a98303c91ce27dc2dc76c822
alphameo <pavel.2014.play@gmail.com>
2026-09-08 14:47:17 +0300

    nvim: upd completion cmds

--- actual/.config/nvim/lua/cmds.lua
+++ actual/.config/nvim/lua/cmds.lua
@@ -29,10 +29,7 @@ vim.api.nvim_create_autocmd("BufReadPost", {
 --     local client = vim.lsp.get_client_by_id(args.data.client_id)
 --     if client ~= nil and client:supports_method "textDocument/completion" then
 --       vim.lsp.completion.enable(true, client.id, args.buf, {
---         autotrigger = true,
---         convert = function(item)
---           return { abbr = item.label }
---         end,
+--         autotrigger = false,
 --       })
 --     end
 --   end,
