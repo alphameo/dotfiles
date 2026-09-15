29737d5f1f62d86386be31630cff947c7e2e5db7
alphameo <pavel.2014.play@gmail.com>
2026-09-15 15:25:05 +0300

    nvim: define latexmk flags

--- actual/.config/nvim/lua/plugins/lang-tex.lua
+++ actual/.config/nvim/lua/plugins/lang-tex.lua
@@ -7,11 +7,27 @@ return {
     ft = ft,
     config = function()
       vim.g.vimtex_mappings_disable = { ["n"] = { "K" } } -- disable `K` as it conflicts with LSP hover
+
       vim.g.vimtex_quickfix_method = vim.fn.executable "pplatex" == 1 and "pplatex" or "latexlog"
+
       -- vim.g.vimtex_view_method = "zathura"
       vim.g.vimtex_view_general_viewer = "okular"
       vim.g.vimtex_view_general_options = "--unique file:@pdf#src:@line@tex"
+
+      vim.g.vimtex_compiler_method = "latexmk"
+      vim.g.vimtex_compiler_latexmk_engines = {
+        _ = "-xelatex",
+      }
+      vim.g.vimtex_compiler_latexmk = {
+        options = {
+          "-file-line-error",
+          "-synctex=1",
+          "-interaction=nonstopmode",
+        },
+      }
+
       vim.g.maplocalleader = " "
+
       vim.o.foldmethod = "expr"
       vim.o.foldexpr = "vimtex#fold#level(v:lnum)"
       vim.o.foldtext = "vimtex#fold#text()"
